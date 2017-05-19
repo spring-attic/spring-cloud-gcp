dd44846bcf29663c28e6e7e0a494fd96ce2e6b50
João André Martins <joaomartins@google.com>
2017-05-19 17:54:48 -0400

    Initial version of Google Cloud Pub/Sub Spring Integration channel adapters.

---
+++ spring-cloud-gcp-pubsub/src/main/java/org/springframework/cloud/gcp/pubsub/integration/inbound/PubSubInboundChannelAdapter.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2017 original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.gcp.pubsub.integration.inbound;
+
+import com.google.cloud.pubsub.spi.v1.AckReplyConsumer;
+import com.google.cloud.pubsub.spi.v1.Subscriber;
+import com.google.pubsub.v1.PubsubMessage;
+import com.google.pubsub.v1.SubscriptionName;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.cloud.gcp.support.GcpHeaders;
+import org.springframework.integration.endpoint.MessageProducerSupport;
+import org.springframework.messaging.MessageHeaders;
+
+/**
+ * Converts from GCP Pub/Sub message to Spring message and sends the Spring message to the
+ * attached channels.
+ *
+ * @author João André Martins
+ */
+public class PubSubInboundChannelAdapter extends MessageProducerSupport {
+
+  private String projectId;
+  private String subscriptionName;
+  private Subscriber subscriber;
+
+  public PubSubInboundChannelAdapter(String projectId, String subscriptionName) {
+    this.projectId = projectId;
+    this.subscriptionName = subscriptionName;
+  }
+
+  @Override
+  protected void doStart() {
+    super.doStart();
+
+    subscriber = Subscriber.defaultBuilder(SubscriptionName.create(
+        projectId, this.subscriptionName), this::receiveMessage).build();
+    subscriber.startAsync();
+  }
+
+  private void receiveMessage(PubsubMessage message, AckReplyConsumer consumer) {
+    Map<String, Object> messageHeaders = new HashMap<>();
+
+    message.getAttributesMap().forEach(messageHeaders::put);
+    // Send the consumer downstream so user decides on when to ack/nack.
+    messageHeaders.put(GcpHeaders.CONSUMER, consumer);
+
+    sendMessage(
+        getMessagingTemplate().getMessageConverter().toMessage(message.getData(),
+            new MessageHeaders(messageHeaders)));
+  }
+
+  @Override
+  protected void doStop() {
+    if (subscriber != null) {
+      subscriber.stopAsync();
+    }
+
+    super.doStop();
+  }
+}
