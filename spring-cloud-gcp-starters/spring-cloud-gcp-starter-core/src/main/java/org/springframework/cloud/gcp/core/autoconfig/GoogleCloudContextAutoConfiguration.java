c9425aef4cc329b024b5a3f718a424bdaab803ba
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2017-05-30 17:11:36 -0400

    Renames core classes  - Adding new Property binding class  - Renaming base autoconfiguration class to reflect a more general    context configuration, not only credentials

--- spring-cloud-gcp-starters/spring-cloud-gcp-starter-core/src/main/java/org/springframework/cloud/gcp/core/autoconfig/GoogleCloudContextAutoConfiguration.java
+++ spring-cloud-gcp-starters/spring-cloud-gcp-starter-core/src/main/java/org/springframework/cloud/gcp/core/autoconfig/GoogleCloudContextAutoConfiguration.java
@@ -2,7 +2,10 @@ package org.springframework.cloud.gcp.core.autoconfig;
 
 import java.io.FileInputStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gcp.core.GoogleCloudProperties;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,26 +17,26 @@ import com.google.auth.oauth2.GoogleCredentials;
 
 /**
  * @author Vinicius Carvalho
+ *
+ * Base starter for Google Cloud Projects. Provide defaults for {@link GoogleCredentials}.
+ * Binds properties from {@link org.springframework.cloud.gcp.core.GoogleCloudProperties}
+ *
  */
 @Configuration
 @ConditionalOnClass(GoogleCredentials.class)
-public class CredentialsAutoConfiguration implements EnvironmentAware {
+@EnableConfigurationProperties(GoogleCloudProperties.class)
+public class GoogleCloudContextAutoConfiguration {
 
-	private static final String AUTH_LOCATION_KEY = "cloud.gcp.auth.location";
 
-	private Environment environment;
-
-	@Override
-	public void setEnvironment(Environment environment) {
-		this.environment = environment;
-	}
+	@Autowired
+	private GoogleCloudProperties googleCloudProperties;
 
 	@Bean
 	public GoogleCredentials googleCredentials(ResourceLoader resourceLoader)
 			throws Exception {
-		if (!StringUtils.isEmpty(this.environment.getProperty(AUTH_LOCATION_KEY))) {
+		if (!StringUtils.isEmpty(this.googleCloudProperties.getJsonKeyFile())) {
 			return GoogleCredentials.fromStream(new FileInputStream(resourceLoader
-					.getResource(this.environment.getProperty(AUTH_LOCATION_KEY))
+					.getResource(this.googleCloudProperties.getJsonKeyFile())
 					.getFile()));
 		}
 		else {
