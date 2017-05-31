0b006522bb9749d22ece71d02a6eed7b735c7675
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2017-05-31 17:45:40 -0400

    Renaming to GCP

--- spring-cloud-gcp-starters/spring-cloud-gcp-starter-core/src/main/java/org/springframework/cloud/gcp/core/GCPProperties.java
+++ spring-cloud-gcp-starters/spring-cloud-gcp-starter-core/src/main/java/org/springframework/cloud/gcp/core/GCPProperties.java
@@ -23,7 +23,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @author Vinicius Carvalho
  */
 @ConfigurationProperties("cloug.gcp")
-public class GoogleCloudProperties {
+public class GCPProperties {
 
 	private String projectId;
 
--- spring-cloud-gcp-starters/spring-cloud-gcp-starter-core/src/main/java/org/springframework/cloud/gcp/core/autoconfig/GCPContextAutoConfiguration.java
+++ spring-cloud-gcp-starters/spring-cloud-gcp-starter-core/src/main/java/org/springframework/cloud/gcp/core/autoconfig/GCPContextAutoConfiguration.java
@@ -5,11 +5,9 @@ import java.io.FileInputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.gcp.core.GoogleCloudProperties;
-import org.springframework.context.EnvironmentAware;
+import org.springframework.cloud.gcp.core.GCPProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.util.StringUtils;
 
@@ -19,24 +17,24 @@ import com.google.auth.oauth2.GoogleCredentials;
  * @author Vinicius Carvalho
  *
  * Base starter for Google Cloud Projects. Provide defaults for {@link GoogleCredentials}.
- * Binds properties from {@link org.springframework.cloud.gcp.core.GoogleCloudProperties}
+ * Binds properties from {@link GCPProperties}
  *
  */
 @Configuration
 @ConditionalOnClass(GoogleCredentials.class)
-@EnableConfigurationProperties(GoogleCloudProperties.class)
-public class GoogleCloudContextAutoConfiguration {
+@EnableConfigurationProperties(GCPProperties.class)
+public class GCPContextAutoConfiguration {
 
 
 	@Autowired
-	private GoogleCloudProperties googleCloudProperties;
+	private GCPProperties gcpProperties;
 
 	@Bean
 	public GoogleCredentials googleCredentials(ResourceLoader resourceLoader)
 			throws Exception {
-		if (!StringUtils.isEmpty(this.googleCloudProperties.getJsonKeyFile())) {
+		if (!StringUtils.isEmpty(this.gcpProperties.getJsonKeyFile())) {
 			return GoogleCredentials.fromStream(new FileInputStream(resourceLoader
-					.getResource(this.googleCloudProperties.getJsonKeyFile())
+					.getResource(this.gcpProperties.getJsonKeyFile())
 					.getFile()));
 		}
 		else {
