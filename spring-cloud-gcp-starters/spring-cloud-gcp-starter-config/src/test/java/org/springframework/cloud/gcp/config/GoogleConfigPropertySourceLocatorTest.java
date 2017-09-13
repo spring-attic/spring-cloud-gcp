ba88e9b2e9fe904557a67ae06b4e8df777655f40
Jisha Abubaker <jishaa@google.com>
2017-09-13 14:05:55 -0700

    removing unused import in test class

--- spring-cloud-gcp-starters/spring-cloud-gcp-starter-config/src/test/java/org/springframework/cloud/gcp/config/GoogleConfigPropertySourceLocatorTest.java
+++ spring-cloud-gcp-starters/spring-cloud-gcp-starter-config/src/test/java/org/springframework/cloud/gcp/config/GoogleConfigPropertySourceLocatorTest.java
@@ -31,7 +31,6 @@ import org.springframework.core.env.PropertySource;
 import org.springframework.core.env.StandardEnvironment;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
