e67510942027d0d9cfa6cdd7e9ba823e5ff6afbc
João Martins <joaoandremartins@users.noreply.github.com>
2017-09-28 10:54:14 -0400

    Adds Spring Integration Channel Adapters for GCP Pub/Sub code sample (#168)
    Adds a couple of applications, one that sends a message and another one
    that receives and processes it by logging it.
    Also slightly fixes other code samples.
    
    Fixes #49 and #146 partially.

---
+++ spring-cloud-gcp-examples/spring-cloud-gcp-integration-pubsub-example/spring-cloud-gcp-integration-pubsub-example-sender/src/main/java/com/example/SenderApplication.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright 2017 original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.example;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gcp.pubsub.core.PubSubOperations;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.gcp.pubsub.outbound.PubSubMessageHandler;
+import org.springframework.messaging.MessageHandler;
+
+/**
+ * Spring Integration Channel Adapters for Google Cloud Pub/Sub code sample.
+ *
+ * @author João André Martins
+ */
+@SpringBootApplication
+public class SenderApplication {
+
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(SenderApplication.class, args);
+	}
+
+	@Bean
+	@ServiceActivator(inputChannel = "pubsubOutputChannel")
+	public MessageHandler messageSender(PubSubOperations pubsubTemplate) {
+		PubSubMessageHandler outboundAdapter =
+				new PubSubMessageHandler(pubsubTemplate, "exampleTopic");
+		// Lets us see errors if there are any.
+		outboundAdapter.setSync(true);
+		return outboundAdapter;
+	}
+
+	@MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
+	public interface PubsubOutboundGateway {
+
+		void sendToPubsub(String text);
+	}
+}
