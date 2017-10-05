85de94b7bfd3895b0d6e417053372d02fce46289
ChengyuanZhao <cccz@google.com>
2017-10-05 16:28:32 -0400

    style fixes

--- spring-cloud-gcp-storage/src/main/java/org/springframework/cloud/gcp/storage/GoogleStorageResource.java
+++ spring-cloud-gcp-storage/src/main/java/org/springframework/cloud/gcp/storage/GoogleStorageResource.java
@@ -16,7 +16,6 @@
 
 package org.springframework.cloud.gcp.storage;
 
-import com.google.cloud.storage.BlobInfo;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -28,6 +27,7 @@ import java.nio.channels.Channels;
 
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 
 import org.springframework.core.io.Resource;
@@ -35,142 +35,138 @@ import org.springframework.core.io.WritableResource;
 import org.springframework.util.Assert;
 
 /**
- * Implements {@link WritableResource} for reading and writing objects in Google Cloud
- * Storage.
+ * Implements {@link WritableResource} for reading and writing objects in Google Cloud Storage.
  *
  * @author Vinicius Carvalho
  * @author Mike Eltsufin
  */
 public class GoogleStorageResource implements WritableResource {
 
-	private final Storage storage;
+  private final Storage storage;
 
-	private final String location;
+  private final String location;
 
-	private boolean createBlobIfNotExists = true;
+  private boolean createBlobIfNotExists = true;
 
-	public GoogleStorageResource(Storage storage, String location, boolean createBlobIfNotExists) {
-		Assert.notNull(storage, "Storage object can not be null");
-		this.storage = storage;
-		this.location = location;
-		this.createBlobIfNotExists = createBlobIfNotExists;
-	}
+  public GoogleStorageResource(Storage storage, String location, boolean createBlobIfNotExists) {
+    Assert.notNull(storage, "Storage object can not be null");
+    this.storage = storage;
+    this.location = location;
+    this.createBlobIfNotExists = createBlobIfNotExists;
+  }
 
-	public GoogleStorageResource(Storage storage, String location) {
-		this(storage, location, true);
-	}
+  public GoogleStorageResource(Storage storage, String location) {
+    this(storage, location, true);
+  }
 
-	public boolean isCreateBlobIfNotExists() {
-		return createBlobIfNotExists;
-	}
+  public boolean isCreateBlobIfNotExists() {
+    return this.createBlobIfNotExists;
+  }
 
-	public void setCreateBlobIfNotExists(boolean createBlobIfNotExists) {
-		this.createBlobIfNotExists = createBlobIfNotExists;
-	}
+  public void setCreateBlobIfNotExists(boolean createBlobIfNotExists) {
+    this.createBlobIfNotExists = createBlobIfNotExists;
+  }
 
-	@Override
-	public boolean exists() {
-		try {
-			return throwExceptionForNullBlob(resolve()).exists();
-		}
-		catch (IOException e) {
-			return false;
-		}
-	}
+  @Override
+  public boolean exists() {
+    try {
+      return throwExceptionForNullBlob(resolve()).exists();
+    } catch (IOException e) {
+      return false;
+    }
+  }
 
-	@Override
-	public boolean isReadable() {
-		return true;
-	}
+  @Override
+  public boolean isReadable() {
+    return true;
+  }
 
-	@Override
-	public boolean isOpen() {
-		return false;
-	}
+  @Override
+  public boolean isOpen() {
+    return false;
+  }
 
-	@Override
-	public URL getURL() throws IOException {
-		return getURI().toURL();
-	}
+  @Override
+  public URL getURL() throws IOException {
+    return getURI().toURL();
+  }
 
-	@Override
-	public URI getURI() throws IOException {
-		URI uri = null;
-		try {
-			uri = new URI(this.location);
-		}
-		catch (URISyntaxException e) {
-			throw new IOException("Invalid URI syntax", e);
-		}
-		return uri;
-	}
+  @Override
+  public URI getURI() throws IOException {
+    URI uri = null;
+    try {
+      uri = new URI(this.location);
+    } catch (URISyntaxException e) {
+      throw new IOException("Invalid URI syntax", e);
+    }
+    return uri;
+  }
 
-	private Blob resolve() throws IOException {
-		URI uri = getURI();
-		BlobId blobId = BlobId.of(uri.getHost(),
-				uri.getPath().substring(1, uri.getPath().length()));
-		Blob blob = this.storage.get(blobId);
-		if ((blob == null || !blob.exists()) && createBlobIfNotExists) {
-			return this.storage.create(BlobInfo.newBuilder(blobId).build());
-		}
-		return blob;
-	}
+  private Blob resolve() throws IOException {
+    URI uri = getURI();
+    BlobId blobId = BlobId.of(uri.getHost(),
+        uri.getPath().substring(1, uri.getPath().length()));
+    Blob blob = this.storage.get(blobId);
+    if ((blob == null || !blob.exists()) && this.createBlobIfNotExists) {
+      return this.storage.create(BlobInfo.newBuilder(blobId).build());
+    }
+    return blob;
+  }
 
-	private Blob throwExceptionForNullBlob(Blob blob) throws IOException {
-		if (blob == null) {
-			throw new IOException("The blob was not found and is null.");
-		}
-		return blob;
-	}
+  private Blob throwExceptionForNullBlob(Blob blob) throws IOException {
+    if (blob == null) {
+      throw new IOException("The blob was not found and is null.");
+    }
+    return blob;
+  }
 
-	@Override
-	public File getFile() throws IOException {
-		throw new UnsupportedOperationException(
-				getDescription() + " cannot be resolved to absolute file path");
-	}
+  @Override
+  public File getFile() throws IOException {
+    throw new UnsupportedOperationException(
+        getDescription() + " cannot be resolved to absolute file path");
+  }
 
-	@Override
-	public long contentLength() throws IOException {
-		return throwExceptionForNullBlob(resolve()).getSize();
-	}
+  @Override
+  public long contentLength() throws IOException {
+    return throwExceptionForNullBlob(resolve()).getSize();
+  }
 
-	@Override
-	public long lastModified() throws IOException {
-		return throwExceptionForNullBlob(resolve()).getUpdateTime();
-	}
+  @Override
+  public long lastModified() throws IOException {
+    return throwExceptionForNullBlob(resolve()).getUpdateTime();
+  }
 
-	@Override
-	public Resource createRelative(String relativePath) throws IOException {
-		throw new UnsupportedOperationException("Directory creation not supported");
-	}
+  @Override
+  public Resource createRelative(String relativePath) throws IOException {
+    throw new UnsupportedOperationException("Directory creation not supported");
+  }
 
-	@Override
-	public String getFilename() {
-		try {
-			return throwExceptionForNullBlob(resolve()).getName();
-		}
-		catch (IOException e) {
-			return null;
-		}
-	}
+  @Override
+  public String getFilename() {
+    try {
+      return throwExceptionForNullBlob(resolve()).getName();
+    } catch (IOException e) {
+      return null;
+    }
+  }
 
-	@Override
-	public String getDescription() {
-		return this.location;
-	}
+  @Override
+  public String getDescription() {
+    return this.location;
+  }
 
-	@Override
-	public InputStream getInputStream() throws IOException {
-		return Channels.newInputStream(throwExceptionForNullBlob(resolve()).reader());
-	}
+  @Override
+  public InputStream getInputStream() throws IOException {
+    return Channels.newInputStream(throwExceptionForNullBlob(resolve()).reader());
+  }
 
-	@Override
-	public boolean isWritable() {
-		return true;
-	}
+  @Override
+  public boolean isWritable() {
+    return true;
+  }
 
-	@Override
-	public OutputStream getOutputStream() throws IOException {
-		return Channels.newOutputStream(throwExceptionForNullBlob(resolve()).writer());
-	}
+  @Override
+  public OutputStream getOutputStream() throws IOException {
+    return Channels.newOutputStream(throwExceptionForNullBlob(resolve()).writer());
+  }
 }
