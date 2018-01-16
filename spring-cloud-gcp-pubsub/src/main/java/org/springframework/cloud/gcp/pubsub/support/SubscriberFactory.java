6ceabb50d21d54066c5cf3366fc50f4b7be837ec
João Martins <joaoandremartins@users.noreply.github.com>
2018-01-16 17:09:03 -0500

    Adds synchronous pull to PubSubTemplate (#283)

--- spring-cloud-gcp-pubsub/src/main/java/org/springframework/cloud/gcp/pubsub/support/SubscriberFactory.java
+++ spring-cloud-gcp-pubsub/src/main/java/org/springframework/cloud/gcp/pubsub/support/SubscriberFactory.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2017 original author or authors.
+ *  Copyright 2017-2018 original author or authors.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -16,15 +16,46 @@
 
 package org.springframework.cloud.gcp.pubsub.support;
 
+import com.google.api.gax.retrying.RetrySettings;
 import com.google.cloud.pubsub.v1.MessageReceiver;
 import com.google.cloud.pubsub.v1.Subscriber;
+import com.google.cloud.pubsub.v1.stub.SubscriberStub;
+import com.google.pubsub.v1.PullRequest;
 
 /**
- * Generates {@link Subscriber} objects.
+ * Interface used by the {@link org.springframework.cloud.gcp.pubsub.core.PubSubTemplate} to create supporting
+ * objects for consuming messages from Pub/Sub subscriptions.
  *
  * @author João André Martins
+ * @author Mike Eltsufin
  */
 public interface SubscriberFactory {
 
-	Subscriber getSubscriber(String subscriptionName, MessageReceiver receiver);
+	/**
+	 * Create a {@link Subscriber} for the specified subscription name and wired it up to asynchronously
+	 * deliver messages to the provided {@link MessageReceiver}.
+	 * @param subscriptionName the name of the subscription
+	 * @param receiver the callback for receiving messages asynchronously
+	 * @return the {@link Subscriber} that was created to bind the receiver to the subscription
+	 */
+	Subscriber createSubscriber(String subscriptionName, MessageReceiver receiver);
+
+	/**
+	 * Create a {@link PullRequest} for synchronously pulling a number of messages from
+	 * a Google Cloud Pub/Sub subscription.
+	 * @param subscriptionName the name of the subscription
+	 * @param maxMessages the maximum number of pulled messages
+	 * @param returnImmediately causes the pull request to return immediately even
+	 * if subscription doesn't contain enough messages to satisfy {@code maxMessages}
+	 * @return the pull request that can be executed using a {@link SubscriberStub}
+	 */
+	PullRequest createPullRequest(String subscriptionName, Integer maxMessages, Boolean returnImmediately);
+
+	/**
+	 * Create a {@link SubscriberStub} that is needed to execute {@link PullRequest}s.
+	 * @param retrySettings parameters for retrying pull requests when they fail, including
+	 * jitter logic, timeout, and exponential backoff
+	 * @return the {@link SubscriberStub} used for executing {@link PullRequest}s
+	 */
+	SubscriberStub createSubscriberStub(RetrySettings retrySettings);
 }
