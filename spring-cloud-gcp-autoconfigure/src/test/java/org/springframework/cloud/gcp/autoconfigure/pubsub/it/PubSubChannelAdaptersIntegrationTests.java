265f18acf82faa79b67cf59fc492ca59606a2d56
João André Martins <joaomartins@google.com>
2018-03-29 12:04:30 -0400

    Switches to context runner

--- spring-cloud-gcp-autoconfigure/src/test/java/org/springframework/cloud/gcp/autoconfigure/pubsub/it/PubSubChannelAdaptersIntegrationTests.java
+++ spring-cloud-gcp-autoconfigure/src/test/java/org/springframework/cloud/gcp/autoconfigure/pubsub/it/PubSubChannelAdaptersIntegrationTests.java
@@ -24,19 +24,16 @@ import com.google.api.gax.core.ExecutorProvider;
 import com.google.api.gax.core.NoCredentialsProvider;
 import com.google.api.gax.rpc.TransportChannelProvider;
 import com.google.cloud.pubsub.v1.AckReplyConsumer;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.cloud.gcp.autoconfigure.pubsub.GcpPubSubAutoConfiguration;
 import org.springframework.cloud.gcp.autoconfigure.pubsub.GcpPubSubEmulatorConfiguration;
-import org.springframework.cloud.gcp.autoconfigure.pubsub.GcpPubSubProperties;
 import org.springframework.cloud.gcp.core.GcpProjectIdProvider;
 import org.springframework.cloud.gcp.core.UsageTrackingHeaderProvider;
 import org.springframework.cloud.gcp.pubsub.PubSubAdmin;
@@ -58,9 +55,7 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.PollableChannel;
-import org.springframework.messaging.converter.StringMessageConverter;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -72,147 +67,122 @@ import static org.mockito.Mockito.verify;
 /**
  * @author João André Martins
  */
-@SpringBootTest(
-		properties = {"spring.cloud.gcp.pubsub.emulatorHost=${PUBSUB_EMULATOR_HOST}"},
-		classes = {PubSubChannelAdaptersIntegrationTests.IntegrationConfiguration.class,
-				GcpPubSubEmulatorConfiguration.class,
-				GcpPubSubAutoConfiguration.class
-		}
-)
-@RunWith(SpringRunner.class)
 public class PubSubChannelAdaptersIntegrationTests {
 
 	private static final String EMULATOR_HOST_ENVVAR_NAME = "PUBSUB_EMULATOR_HOST";
 
-	@Autowired
-	@Qualifier("outputChannel")
-	private PollableChannel channel;
-
-	@Autowired
-	private PubSubAdmin pubSubAdmin;
-
-	@Autowired
-	private GcpPubSubProperties pubSubProperties;
-
-	@Autowired
-	@Qualifier("inputChannel")
-	private MessageChannel inputChannel;
-
-	@Autowired
-	private PubSubInboundChannelAdapter inboundChannelAdapter;
-
-	@Autowired
-	private PubSubMessageHandler outboundChannelAdapter;
-
-	// Increment for each added test.
-	// Used in a JUnit5 @AfterAll way, to clean up the Pub/Sub environment after all tests ran.
-	// Can't use @AfterClass, since the PubSubAdmin bean isn't ready at static time.
-	private static final int TOTAL_TESTS = 4;
-
-	private static int testsRan;
+	private ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+			.withConfiguration(AutoConfigurations.of(
+					GcpPubSubEmulatorConfiguration.class,
+					GcpPubSubAutoConfiguration.class))
+			.withUserConfiguration(
+					PubSubChannelAdaptersIntegrationTests.IntegrationConfiguration.class)
+			.withPropertyValues("spring.cloud.gcp.pubsub.emulatorHost=${PUBSUB_EMULATOR_HOST}");
 
 	@BeforeClass
 	public static void checkEmulatorIsRunning() {
 		assumeThat(System.getenv(EMULATOR_HOST_ENVVAR_NAME)).isNotNull();
 	}
 
-	@Before
-	public void setUp() {
-		// Sets the defaults of the fields we'll change in later tests, so we don't need to use
-		// @DirtiesContext, which is performance unfriendly.
-		StringMessageConverter stringMessageConverter = new StringMessageConverter();
-		stringMessageConverter.setSerializedPayloadClass(String.class);
-		this.inboundChannelAdapter.setMessageConverter(stringMessageConverter);
-		this.inboundChannelAdapter.setAckMode(AckMode.AUTO);
-		this.outboundChannelAdapter.setPublishCallback(null);
-	}
-
-	@After
-	public void tearDown() {
-		if (testsRan == TOTAL_TESTS) {
-			this.pubSubAdmin.deleteTopic("desafinado");
-			this.pubSubAdmin.deleteSubscription("doralice");
-		}
+	private void tearDown(PubSubAdmin pubSubAdmin) {
+		pubSubAdmin.deleteTopic("desafinado");
+		pubSubAdmin.deleteSubscription("doralice");
 	}
 
 	@Test
 	public void sendAndReceiveMessage() {
-		testsRan++;
-		Map<String, Object> headers = new HashMap<>();
-		// Only String values for now..
-		headers.put("storm", "lift your skinny fists");
-		headers.put("static", "lift your skinny fists");
-		headers.put("sleep", "lift your skinny fists");
+		this.contextRunner.run(context -> {
+			Map<String, Object> headers = new HashMap<>();
+			// Only String values for now..
+			headers.put("storm", "lift your skinny fists");
+			headers.put("static", "lift your skinny fists");
+			headers.put("sleep", "lift your skinny fists");
 
-		this.inputChannel.send(
-				MessageBuilder.createMessage("I am a message.",  new MessageHeaders(headers)));
+			context.getBean("inputChannel", MessageChannel.class).send(
+					MessageBuilder.createMessage("I am a message.",  new MessageHeaders(headers)));
 
-		Message<?> message = this.channel.receive(5000);
-		assertThat(message).isNotNull();
-		assertThat(message.getPayload()).isInstanceOf(String.class);
-		String payload = (String) message.getPayload();
-		assertThat(payload).isEqualTo("I am a message.");
+			Message<?> message =
+					context.getBean("outputChannel", PollableChannel.class).receive(5000);
+			assertThat(message).isNotNull();
+			assertThat(message.getPayload()).isInstanceOf(String.class);
+			String payload = (String) message.getPayload();
+			assertThat(payload).isEqualTo("I am a message.");
 
-		assertThat(message.getHeaders().size()).isEqualTo(6);
-		assertThat(message.getHeaders().get("storm")).isEqualTo("lift your skinny fists");
-		assertThat(message.getHeaders().get("static")).isEqualTo("lift your skinny fists");
-		assertThat(message.getHeaders().get("sleep")).isEqualTo("lift your skinny fists");
+			assertThat(message.getHeaders().size()).isEqualTo(6);
+			assertThat(message.getHeaders().get("storm")).isEqualTo("lift your skinny fists");
+			assertThat(message.getHeaders().get("static")).isEqualTo("lift your skinny fists");
+			assertThat(message.getHeaders().get("sleep")).isEqualTo("lift your skinny fists");
+			tearDown(context.getBean(PubSubAdmin.class));
+		});
 	}
 
 	@Test
 	public void sendAndReceiveMessageInBytes() {
-		testsRan++;
-		this.inboundChannelAdapter.setMessageConverter(null);
-		this.inputChannel.send(MessageBuilder.withPayload("I am a message.").build());
+		this.contextRunner.run(context -> {
+			context.getBean(PubSubInboundChannelAdapter.class).setMessageConverter(null);
+			context.getBean("inputChannel", MessageChannel.class).send(
+					MessageBuilder.withPayload("I am a message.").build());
 
-		Message<?> message = this.channel.receive(5000);
-		assertThat(message).isNotNull();
-		assertThat(message.getPayload()).isInstanceOf(byte[].class);
-		String stringPayload = new String((byte[]) message.getPayload());
-		assertThat(stringPayload).isEqualTo("I am a message.");
+			Message<?> message =
+					context.getBean("outputChannel", PollableChannel.class).receive(5000);
+			assertThat(message).isNotNull();
+			assertThat(message.getPayload()).isInstanceOf(byte[].class);
+			String stringPayload = new String((byte[]) message.getPayload());
+			assertThat(stringPayload).isEqualTo("I am a message.");
+			tearDown(context.getBean(PubSubAdmin.class));
+		});
 	}
 
 	@Test
 	public void sendAndReceiveMessageManualAck() {
-		testsRan++;
-		this.inboundChannelAdapter.setAckMode(AckMode.MANUAL);
-		this.inputChannel.send(MessageBuilder.withPayload("I am a message.").build());
+		this.contextRunner.run(context -> {
+			context.getBean(PubSubInboundChannelAdapter.class).setAckMode(AckMode.MANUAL);
+			context.getBean("inputChannel", MessageChannel.class).send(
+					MessageBuilder.withPayload("I am a message.").build());
 
-		Message<?> message = this.channel.receive(5000);
-		assertThat(message).isNotNull();
-		AckReplyConsumer acker =
-				(AckReplyConsumer) message.getHeaders().get(GcpHeaders.ACKNOWLEDGEMENT);
-		assertThat(acker).isNotNull();
-		acker.nack();
-		message = this.channel.receive(1000);
-		assertThat(message).isNotNull();
-		acker = (AckReplyConsumer) message.getHeaders().get(GcpHeaders.ACKNOWLEDGEMENT);
-		assertThat(acker).isNotNull();
-		acker.ack();
-		message = this.channel.receive(1000);
-		assertThat(message).isNull();
+			PollableChannel channel = context.getBean("outputChannel", PollableChannel.class);
+
+			Message<?> message = channel.receive(5000);
+			assertThat(message).isNotNull();
+			AckReplyConsumer acker =
+					(AckReplyConsumer) message.getHeaders().get(GcpHeaders.ACKNOWLEDGEMENT);
+			assertThat(acker).isNotNull();
+			acker.nack();
+			message = channel.receive(1000);
+			assertThat(message).isNotNull();
+			acker = (AckReplyConsumer) message.getHeaders().get(GcpHeaders.ACKNOWLEDGEMENT);
+			assertThat(acker).isNotNull();
+			acker.ack();
+			message = channel.receive(1000);
+			assertThat(message).isNull();
+			tearDown(context.getBean(PubSubAdmin.class));
+		});
 	}
 
 	@Test
 	public void sendAndReceiveMessagePublishCallback() {
-		testsRan++;
-		ListenableFutureCallback<String> callbackSpy = Mockito.spy(new ListenableFutureCallback<String>() {
-			@Override
-			public void onFailure(Throwable ex) {
+		this.contextRunner.run(context -> {
+			ListenableFutureCallback<String> callbackSpy = Mockito.spy(new ListenableFutureCallback<String>() {
+				@Override
+				public void onFailure(Throwable ex) {
 
-			}
+				}
 
-			@Override
-			public void onSuccess(String result) {
+				@Override
+				public void onSuccess(String result) {
 
-			}
+				}
+			});
+			context.getBean(PubSubMessageHandler.class).setPublishCallback(callbackSpy);
+			context.getBean("inputChannel", MessageChannel.class).send(
+					MessageBuilder.withPayload("I am a message.").build());
+
+			Message<?> message =
+					context.getBean("outputChannel", PollableChannel.class).receive(5000);
+			assertThat(message).isNotNull();
+			verify(callbackSpy, times(1)).onSuccess(any());
+			tearDown(context.getBean(PubSubAdmin.class));
 		});
-		this.outboundChannelAdapter.setPublishCallback(callbackSpy);
-		this.inputChannel.send(MessageBuilder.withPayload("I am a message.").build());
-
-		Message<?> message = this.channel.receive(5000);
-		assertThat(message).isNotNull();
-		verify(callbackSpy, times(1)).onSuccess(any());
 	}
 
 	@Configuration
