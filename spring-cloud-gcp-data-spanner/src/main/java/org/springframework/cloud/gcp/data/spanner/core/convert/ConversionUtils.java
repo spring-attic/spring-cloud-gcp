cf59d49c0ff5031e97f3c920ef44589b4cbee13c
ChengyuanZhao <cccz@google.com>
2018-04-06 08:48:06 -0400

    checkstyle fix

--- spring-cloud-gcp-data-spanner/src/main/java/org/springframework/cloud/gcp/data/spanner/core/convert/ConversionUtils.java
+++ spring-cloud-gcp-data-spanner/src/main/java/org/springframework/cloud/gcp/data/spanner/core/convert/ConversionUtils.java
@@ -36,7 +36,6 @@ import com.google.common.collect.ImmutableSet;
 
 import org.springframework.cloud.gcp.data.spanner.core.mapping.SpannerDataException;
 import org.springframework.cloud.gcp.data.spanner.core.mapping.SpannerPersistentProperty;
-import org.springframework.cloud.gcp.data.spanner.core.mapping.SpannerPersistentProperty;
 import org.springframework.cloud.gcp.data.spanner.core.mapping.Table;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.Nullable;
@@ -50,21 +49,112 @@ public class ConversionUtils {
 
 	public static final Map<Type, Class> SPANNER_COLUMN_TYPE_TO_JAVA_TYPE_MAPPING =
 			new ImmutableMap.Builder<Type, Class>()
-			.put(Type.bool(), Boolean.class).put(Type.bytes(), ByteArray.class)
-			.put(Type.date(), com.google.cloud.Date.class)
-			.put(Type.float64(), Double.class).put(Type.int64(), Long.class)
-			.put(Type.string(), String.class)
-			.put(Type.array(Type.float64()), double[].class)
-			.put(Type.array(Type.int64()), long[].class)
-			.put(Type.array(Type.bool()), boolean[].class)
-			.put(Type.timestamp(), Timestamp.class).build();
+					.put(Type.bool(), Boolean.class).put(Type.bytes(), ByteArray.class)
+					.put(Type.date(), com.google.cloud.Date.class)
+					.put(Type.float64(), Double.class).put(Type.int64(), Long.class)
+					.put(Type.string(), String.class)
+					.put(Type.array(Type.float64()), double[].class)
+					.put(Type.array(Type.int64()), long[].class)
+					.put(Type.array(Type.bool()), boolean[].class)
+					.put(Type.timestamp(), Timestamp.class).build();
+
+	public static final Converter<Date, com.google.cloud.Date> JAVA_TO_SPANNER_DATE_CONVERTER =
+			new Converter<Date, com.google.cloud.Date>() {
+				@Nullable
+				@Override
+				public com.google.cloud.Date convert(Date date) {
+					Calendar cal = Calendar.getInstance();
+					cal.setTime(date);
+					return com.google.cloud.Date.fromYearMonthDay(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1,
+							cal.get(Calendar.DAY_OF_MONTH));
+				}
+			};
+
+	public static final Converter<com.google.cloud.Date, Date> SPANNER_TO_JAVA_DATE_CONVERTER =
+			new Converter<com.google.cloud.Date, Date>() {
+				@Nullable
+				@Override
+				public Date convert(com.google.cloud.Date date) {
+					Calendar cal = Calendar.getInstance();
+					cal.set(date.getYear(), date.getMonth() - 1, date.getDayOfMonth());
+					return cal.getTime();
+				}
+			};
+
+	public static final Converter<Instant, Timestamp> INSTANT_TIMESTAMP_CONVERTER =
+			new Converter<Instant, Timestamp>() {
+				@Nullable
+				@Override
+				public Timestamp convert(Instant instant) {
+					return Timestamp.ofTimeSecondsAndNanos(instant.getEpochSecond(), instant.getNano());
+				}
+			};
+
+	public static final Converter<Timestamp, Instant> TIMESTAMP_INSTANT_CONVERTER =
+			new Converter<Timestamp, Instant>() {
+				@Nullable
+				@Override
+				public Instant convert(Timestamp timestamp) {
+					return Instant.ofEpochSecond(timestamp.getSeconds(), timestamp.getNanos());
+				}
+			};
+
+	public static final Converter<java.sql.Timestamp, Timestamp> JAVA_TO_SPANNER_TIMESTAMP_CONVERTER =
+			new Converter<java.sql.Timestamp, Timestamp>() {
+				@Nullable
+				@Override
+				public Timestamp convert(java.sql.Timestamp timestamp) {
+					return Timestamp.of(timestamp);
+				}
+			};
+
+	public static final Converter<Timestamp, java.sql.Timestamp> SPANNER_TO_JAVA_TIMESTAMP_CONVERTER =
+			new Converter<Timestamp, java.sql.Timestamp>() {
+				@Nullable
+				@Override
+				public java.sql.Timestamp convert(Timestamp timestamp) {
+					return java.sql.Timestamp.from(TIMESTAMP_INSTANT_CONVERTER.convert(timestamp));
+				}
+			};
+
+	public static final Converter<byte[], ByteArray> JAVA_TO_SPANNER_BYTE_ARRAY_CONVERTER =
+			new Converter<byte[], ByteArray>() {
+				@Nullable
+				@Override
+				public ByteArray convert(byte[] bytes) {
+					return ByteArray.copyFrom(bytes);
+				}
+			};
+
+	public static final Converter<ByteArray, byte[]> SPANNER_TO_JAVA_BYTE_ARRAY_CONVERTER =
+			new Converter<ByteArray, byte[]>() {
+				@Nullable
+				@Override
+				public byte[] convert(ByteArray bytes) {
+					return bytes.toByteArray();
+				}
+			};
+
+	/**
+	 * Converters from common types to those used by Spanner.
+	 */
+	public static final Collection<Converter> DEFAULT_SPANNER_WRITE_CONVERTERS = ImmutableSet
+			.of(JAVA_TO_SPANNER_DATE_CONVERTER, INSTANT_TIMESTAMP_CONVERTER, JAVA_TO_SPANNER_BYTE_ARRAY_CONVERTER,
+					JAVA_TO_SPANNER_TIMESTAMP_CONVERTER);
+
+	/**
+	 * Converters from common types to those used by Spanner.
+	 */
+	public static final Collection<Converter> DEFAULT_SPANNER_READ_CONVERTERS = ImmutableSet
+			.of(SPANNER_TO_JAVA_DATE_CONVERTER, TIMESTAMP_INSTANT_CONVERTER, SPANNER_TO_JAVA_BYTE_ARRAY_CONVERTER,
+					SPANNER_TO_JAVA_TIMESTAMP_CONVERTER);
 
 	private static final Map<Class, Type> JAVA_TYPE_TO_SPANNER_COLUMN_TYPE_MAPPING;
 
 	static {
 		ImmutableMap.Builder<Class, Type> builder = new Builder<>();
 		SPANNER_COLUMN_TYPE_TO_JAVA_TYPE_MAPPING.keySet().stream().forEach(type -> builder
-						.put(SPANNER_COLUMN_TYPE_TO_JAVA_TYPE_MAPPING.get(type), type));
+				.put(SPANNER_COLUMN_TYPE_TO_JAVA_TYPE_MAPPING.get(type), type));
 		JAVA_TYPE_TO_SPANNER_COLUMN_TYPE_MAPPING = builder.build();
 	}
 
@@ -75,7 +165,7 @@ public class ConversionUtils {
 		}
 		return type.toString()
 				+ (type.getCode() == Code.STRING || type.getCode() == Code.BYTES ? "(MAX)"
-						: "");
+				: "");
 	}
 
 	private static Class getFullyConvertableType(SpannerConverter spannerConverter,
@@ -90,17 +180,17 @@ public class ConversionUtils {
 		for (Class spannerType : spannerTypes) {
 			if (isIterableInnerType
 					&& spannerConverter.canHandlePropertyTypeForArrayRead(originalType,
-							spannerType)
+					spannerType)
 					&& spannerConverter.canHandlePropertyTypeForArrayWrite(originalType,
-							spannerType)) {
+					spannerType)) {
 				ret = spannerType;
 				break;
 			}
 			else if (!isIterableInnerType
 					&& spannerConverter.canHandlePropertyTypeForSingularRead(originalType,
-							spannerType)
+					spannerType)
 					&& spannerConverter.canHandlePropertyTypeForSingularWrite(
-							originalType, spannerType)) {
+					originalType, spannerType)) {
 				ret = spannerType;
 				break;
 			}
@@ -138,100 +228,10 @@ public class ConversionUtils {
 		return getTypeDDLString(spannerColumnType);
 	}
 
-	public static final Converter<Date, com.google.cloud.Date> JAVA_TO_SPANNER_DATE_CONVERTER =
-			new Converter<Date, com.google.cloud.Date>() {
-		@Nullable
-		@Override
-		public com.google.cloud.Date convert(Date date) {
-			Calendar cal = Calendar.getInstance();
-			cal.setTime(date);
-			return com.google.cloud.Date.fromYearMonthDay(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1,
-					cal.get(Calendar.DAY_OF_MONTH));
-		}
-	};
-
-	public static final Converter<com.google.cloud.Date, Date> SPANNER_TO_JAVA_DATE_CONVERTER =
-			new Converter<com.google.cloud.Date, Date>() {
-		@Nullable
-		@Override
-		public Date convert(com.google.cloud.Date date) {
-			Calendar cal = Calendar.getInstance();
-			cal.set(date.getYear(), date.getMonth() - 1, date.getDayOfMonth());
-			return cal.getTime();
-		}
-	};
-
-	public static final Converter<Instant, Timestamp> INSTANT_TIMESTAMP_CONVERTER =
-			new Converter<Instant, Timestamp>() {
-		@Nullable
-		@Override
-		public Timestamp convert(Instant instant) {
-			return Timestamp.ofTimeSecondsAndNanos(instant.getEpochSecond(), instant.getNano());
-		}
-	};
-
-	public static final Converter<Timestamp, Instant> TIMESTAMP_INSTANT_CONVERTER =
-			new Converter<Timestamp, Instant>() {
-		@Nullable
-		@Override
-		public Instant convert(Timestamp timestamp) {
-			return Instant.ofEpochSecond(timestamp.getSeconds(), timestamp.getNanos());
-		}
-	};
-
-	public static final Converter<java.sql.Timestamp, Timestamp> JAVA_TO_SPANNER_TIMESTAMP_CONVERTER =
-			new Converter<java.sql.Timestamp, Timestamp>() {
-		@Nullable
-		@Override
-		public Timestamp convert(java.sql.Timestamp timestamp) {
-			return Timestamp.of(timestamp);
-		}
-	};
-
-	public static final Converter<Timestamp, java.sql.Timestamp> SPANNER_TO_JAVA_TIMESTAMP_CONVERTER =
-			new Converter<Timestamp, java.sql.Timestamp>() {
-		@Nullable
-		@Override
-		public java.sql.Timestamp convert(Timestamp timestamp) {
-			return java.sql.Timestamp.from(TIMESTAMP_INSTANT_CONVERTER.convert(timestamp));
-		}
-	};
-
-	public static final Converter<byte[], ByteArray> JAVA_TO_SPANNER_BYTE_ARRAY_CONVERTER =
-			new Converter<byte[], ByteArray>() {
-		@Nullable
-		@Override
-		public ByteArray convert(byte[] bytes) {
-			return ByteArray.copyFrom(bytes);
-		}
-	};
-
-	public static final Converter<ByteArray, byte[]> SPANNER_TO_JAVA_BYTE_ARRAY_CONVERTER =
-			new Converter<ByteArray, byte[]>() {
-		@Nullable
-		@Override
-		public byte[] convert(ByteArray bytes) {
-			return bytes.toByteArray();
-		}
-	};
-
-	/**
-	 * Converters from common types to those used by Spanner.
-	 */
-	public static final Collection<Converter> DEFAULT_SPANNER_WRITE_CONVERTERS = ImmutableSet
-			.of(JAVA_TO_SPANNER_DATE_CONVERTER, INSTANT_TIMESTAMP_CONVERTER, JAVA_TO_SPANNER_BYTE_ARRAY_CONVERTER,
-					JAVA_TO_SPANNER_TIMESTAMP_CONVERTER);
-
-	/**
-	 * Converters from common types to those used by Spanner.
-	 */
-	public static final Collection<Converter> DEFAULT_SPANNER_READ_CONVERTERS = ImmutableSet
-			.of(SPANNER_TO_JAVA_DATE_CONVERTER, TIMESTAMP_INSTANT_CONVERTER, SPANNER_TO_JAVA_BYTE_ARRAY_CONVERTER,
-					SPANNER_TO_JAVA_TIMESTAMP_CONVERTER);
-
 	/**
 	 * returns true if the given SpannerPersistentProperty refers to entities stored in a
 	 * Spanner table. returns false otherwise.
+	 *
 	 * @param spannerPersistentProperty
 	 * @return
 	 */
