bdbb530d80720bee61b83987d8d846528d2b583e
Mike Eltsufin <meltsufin@google.com>
2018-04-10 16:25:22 -0400

    Return SpannerTemplate instead of SpannerOperations in autoconfig
    Fixes #533.

--- spring-cloud-gcp-data-spanner/src/main/java/org/springframework/cloud/gcp/data/spanner/repository/config/EnableSpannerRepositories.java
+++ spring-cloud-gcp-data-spanner/src/main/java/org/springframework/cloud/gcp/data/spanner/repository/config/EnableSpannerRepositories.java
@@ -120,7 +120,7 @@ public @interface EnableSpannerRepositories {
 	 *
 	 * @return
 	 */
-	String spannerOperationsRef() default "spannerOperations";
+	String spannerOperationsRef() default "spannerTemplate";
 
 	/**
 	 * Configures the name of the
