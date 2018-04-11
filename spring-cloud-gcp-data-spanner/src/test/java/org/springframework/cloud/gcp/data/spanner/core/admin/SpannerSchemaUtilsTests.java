e8ee655403c952a594748be9717ecd7354e4779d
Balint Pato <balopat@users.noreply.github.com>
2018-04-11 09:59:44 -0400

    PrimaryKeyColumn -> PrimaryKey (#548)
    * PrimaryKeyColumn -> PrimaryKey
    
    * missing file

--- spring-cloud-gcp-data-spanner/src/test/java/org/springframework/cloud/gcp/data/spanner/core/admin/SpannerSchemaUtilsTests.java
+++ spring-cloud-gcp-data-spanner/src/test/java/org/springframework/cloud/gcp/data/spanner/core/admin/SpannerSchemaUtilsTests.java
@@ -26,7 +26,7 @@ import org.junit.Test;
 import org.springframework.cloud.gcp.data.spanner.core.convert.MappingSpannerConverter;
 import org.springframework.cloud.gcp.data.spanner.core.mapping.Column;
 import org.springframework.cloud.gcp.data.spanner.core.mapping.ColumnInnerType;
-import org.springframework.cloud.gcp.data.spanner.core.mapping.PrimaryKeyColumn;
+import org.springframework.cloud.gcp.data.spanner.core.mapping.PrimaryKey;
 import org.springframework.cloud.gcp.data.spanner.core.mapping.SpannerMappingContext;
 import org.springframework.cloud.gcp.data.spanner.core.mapping.Table;
 
@@ -60,10 +60,10 @@ public class SpannerSchemaUtilsTests {
 
 	@Table(name = "custom_test_table")
 	private static class TestEntity {
-		@PrimaryKeyColumn(keyOrder = 1)
+		@PrimaryKey(keyOrder = 1)
 		String id;
 
-		@PrimaryKeyColumn(keyOrder = 2)
+		@PrimaryKey(keyOrder = 2)
 		long id2;
 
 		@Column(name = "custom_col")
