dfcc4a0ee3800e321ff4712cd82f57806aef0cb3
ChengyuanZhao <cccz@google.com>
2018-04-19 12:03:09 -0400

    wip

---
+++ spring-cloud-gcp-data-spanner/src/test/java/org/springframework/cloud/gcp/data/spanner/core/convert/MappingSpannerReadConverterTests.java
@@ -0,0 +1,67 @@
+/*
+ *  Copyright 2018 original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.springframework.cloud.gcp.data.spanner.core.convert;
+
+import com.google.cloud.spanner.Struct;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.List;
+import org.junit.Test;
+import org.springframework.cloud.gcp.data.spanner.core.mapping.PrimaryKey;
+import org.springframework.cloud.gcp.data.spanner.core.mapping.SpannerMappingContext;
+import org.springframework.cloud.gcp.data.spanner.core.mapping.SpannerPersistentProperty;
+import org.springframework.data.mapping.PropertyHandler;
+
+/**
+ * @author Chengyuan Zhao
+ */
+public class MappingSpannerReadConverterTests {
+
+  // Checks that the converter is aware of all Spanner struct getter types
+  @Test
+  public void allKnownMappingTypesTest() throws NoSuchFieldException {
+
+    new SpannerMappingContext().getPersistentEntity(Entity.class).doWithProperties(
+        new PropertyHandler<SpannerPersistentProperty>() {
+          @Override
+          public void doWithPersistentProperty(SpannerPersistentProperty persistentProperty) {
+            int x =1;
+          }
+        });
+
+    Type t = Entity.class.getField("test").getGenericType();
+
+    for( Method method: Struct.class.getMethods()){
+
+      // ignoring private methods
+      if(!method.isAccessible()){
+        continue;
+      }
+
+
+      Type returnedType = method.getGenericReturnType();
+    }
+  }
+
+  private class Entity{
+
+    @PrimaryKey
+        int x;
+
+    List<Integer> test;
+  }
+}
