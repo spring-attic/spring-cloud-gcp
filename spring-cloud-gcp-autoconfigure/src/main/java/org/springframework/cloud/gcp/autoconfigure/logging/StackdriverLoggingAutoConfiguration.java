f10ddcbf2ee60392dbe76fc298122a7dc926b37a
João André Martins <joaomartins@google.com>
2018-06-05 13:53:46 -0400

    Checkstyle

--- spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/logging/StackdriverLoggingAutoConfiguration.java
+++ spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/logging/StackdriverLoggingAutoConfiguration.java
@@ -33,7 +33,6 @@ import org.springframework.cloud.sleuth.autoconfig.SleuthProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import zipkin2.reporter.stackdriver.StackdriverSender;
 
 /**
  * @author Mike Eltsufin
