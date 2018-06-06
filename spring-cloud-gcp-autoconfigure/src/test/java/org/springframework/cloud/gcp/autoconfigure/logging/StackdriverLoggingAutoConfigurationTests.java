spring-attic/spring-cloud-gcp

commit db0fdb7711400fb78fd6b2f525fefbc1d562187a

Logging module queries MDC (#714)
If MDC doesn't have any saved trace ID (read: Sleuth isn't used), then
logging resorts to the trace ID manually extracted from the request, in
order to group log entries by trace ID.

Fixes #637

spring-cloud-gcp-autoconfigure/src/test/java/org/springframework/cloud/gcp/autoconfigure/logging/StackdriverLoggingAutoConfigurationTests.java
@@ -28,6 +28,7 @@ import org.springframework.cloud.gcp.autoconfigure.logging.extractors.TraceIdExt
 import org.springframework.cloud.gcp.autoconfigure.logging.extractors.TraceIdExtractorType;
 import org.springframework.cloud.gcp.autoconfigure.logging.extractors.XCloudTraceIdExtractor;
 import org.springframework.cloud.gcp.autoconfigure.logging.extractors.ZipkinTraceIdExtractor;
+import org.springframework.cloud.gcp.autoconfigure.trace.StackdriverTraceAutoConfiguration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -116,5 +117,15 @@ public class StackdriverLoggingAutoConfigurationTests {
 
 		assertThat(new StackdriverLoggingAutoConfiguration().traceIdExtractor(properties))
 				.isInstanceOf(ZipkinTraceIdExtractor.class);
+	}
+
+	@Test
+	public void testWithSleuth() {
+		this.contextRunner
+				.withConfiguration(AutoConfigurations.of(StackdriverTraceAutoConfiguration.class))
+				.run(context -> {
+					assertThat(context.getBeansOfType(TraceIdLoggingWebMvcInterceptor.class).size())
+							.isEqualTo(0);
+				});
 	}
 }

spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/logging/LoggingWebMvcConfigurer.java
@@ -53,8 +53,7 @@ public class LoggingWebMvcConfigurer implements WebMvcConfigurer {
 		else {
 			this.interceptor = new TraceIdLoggingWebMvcInterceptor(
 					new CompositeTraceIdExtractor(
-							new XCloudTraceIdExtractor(), new ZipkinTraceIdExtractor()),
-					projectIdProvider);
+							new XCloudTraceIdExtractor(), new ZipkinTraceIdExtractor()));
 		}
 	}
 

spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/logging/StackdriverJsonLayout.java
@@ -41,50 +41,10 @@ import org.springframework.util.StringUtils;
  */
 public class StackdriverJsonLayout extends JsonLayout {
 
-	/**
-	 * The JSON field name for the log level (severity)
-	 */
-	public static final String SEVERITY_ATTRIBUTE = "severity";
-
-	/**
-	 * The JSON field name for the seconds of the timestamp
-	 */
-	public static final String TIMESTAMP_SECONDS_ATTRIBUTE = "timestampSeconds";
-
-	/**
-	 * The JSON field name for the nanos of the timestamp
-	 */
-	public static final String TIMESTAMP_NANOS_ATTRIBUTE = "timestampNanos";
-
-	/**
-	 * The JSON field name for the span-id
-	 */
-	public static final String SPAN_ID_ATTRIBUTE = "logging.googleapis.com/spanId";
-
-	/**
-	 * The JSON field name for the trace-id
-	 */
-	public static final String TRACE_ID_ATTRIBUTE = "logging.googleapis.com/trace";
-
-	/**
-	 * The name of the MDC parameter, Spring Sleuth is storing the trace id at
-	 */
-	public static final String MDC_FIELD_TRACE_ID = "X-B3-TraceId";
-
-	/**
-	 * The name of the MDC parameter, Spring Sleuth is storing the span id at
-	 */
-	public static final String MDC_FIELD_SPAN_ID = "X-B3-SpanId";
-
-	/**
-	 * The name of the MDC parameter, Spring Sleuth is storing the span export information at
-	 */
-	public static final String MDC_FIELD_SPAN_EXPORT = "X-Span-Export";
-
 	private static final Set<String> FILTERED_MDC_FIELDS = new HashSet<>(Arrays.asList(
-			MDC_FIELD_TRACE_ID,
-			MDC_FIELD_SPAN_ID,
-			MDC_FIELD_SPAN_EXPORT));
+			StackdriverTraceConstants.MDC_FIELD_TRACE_ID,
+			StackdriverTraceConstants.MDC_FIELD_SPAN_ID,
+			StackdriverTraceConstants.MDC_FIELD_SPAN_EXPORT));
 
 	private String projectId;
 
@@ -194,11 +154,14 @@ public class StackdriverJsonLayout extends JsonLayout {
 			});
 		}
 		if (this.includeTimestamp) {
-			map.put(TIMESTAMP_SECONDS_ATTRIBUTE, TimeUnit.MILLISECONDS.toSeconds(event.getTimeStamp()));
-			map.put(TIMESTAMP_NANOS_ATTRIBUTE, TimeUnit.MILLISECONDS.toNanos(event.getTimeStamp() % 1_000));
+			map.put(StackdriverTraceConstants.TIMESTAMP_SECONDS_ATTRIBUTE,
+					TimeUnit.MILLISECONDS.toSeconds(event.getTimeStamp()));
+			map.put(StackdriverTraceConstants.TIMESTAMP_NANOS_ATTRIBUTE,
+					TimeUnit.MILLISECONDS.toNanos(event.getTimeStamp() % 1_000));
 		}
 
-		add(SEVERITY_ATTRIBUTE, this.includeLevel, String.valueOf(event.getLevel()), map);
+		add(StackdriverTraceConstants.SEVERITY_ATTRIBUTE, this.includeLevel,
+				String.valueOf(event.getLevel()), map);
 		add(JsonLayout.THREAD_ATTR_NAME, this.includeThreadName, event.getThreadName(), map);
 		add(JsonLayout.LOGGER_ATTR_NAME, this.includeLoggerName, event.getLoggerName(), map);
 
@@ -219,7 +182,8 @@ public class StackdriverJsonLayout extends JsonLayout {
 		add(JsonLayout.CONTEXT_ATTR_NAME, this.includeContextName, event.getLoggerContextVO().getName(), map);
 		addThrowableInfo(JsonLayout.EXCEPTION_ATTR_NAME, this.includeException, event, map);
 		addTraceId(event, map);
-		add(SPAN_ID_ATTRIBUTE, this.includeSpanId, event.getMDCPropertyMap().get(MDC_FIELD_SPAN_ID), map);
+		add(StackdriverTraceConstants.SPAN_ID_ATTRIBUTE, this.includeSpanId,
+				event.getMDCPropertyMap().get(StackdriverTraceConstants.MDC_FIELD_SPAN_ID), map);
 		addCustomDataToJsonMap(map, event);
 		return map;
 	}
@@ -238,16 +202,18 @@ public class StackdriverJsonLayout extends JsonLayout {
 			return;
 		}
 
-		String traceId = TraceLoggingEnhancer.getCurrentTraceId();
+		String traceId =
+				event.getMDCPropertyMap().get(StackdriverTraceConstants.MDC_FIELD_TRACE_ID);
 		if (traceId == null) {
-			traceId = event.getMDCPropertyMap().get(MDC_FIELD_TRACE_ID);
+			traceId = TraceLoggingEnhancer.getCurrentTraceId();
 		}
 		if (!StringUtils.isEmpty(traceId)
 			&& !StringUtils.isEmpty(this.projectId)
 			&& !this.projectId.endsWith("_IS_UNDEFINED")) {
-			traceId = "projects/" + this.projectId + "/traces/" + formatTraceId(traceId);
+			traceId = StackdriverTraceConstants.composeFullTraceName(
+					this.projectId, formatTraceId(traceId));
 		}
 
-		add(TRACE_ID_ATTRIBUTE, this.includeTraceId, traceId, map);
+		add(StackdriverTraceConstants.TRACE_ID_ATTRIBUTE, this.includeTraceId, traceId, map);
 	}
 }

spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/logging/StackdriverLoggingAutoConfiguration.java
@@ -18,6 +18,7 @@ package org.springframework.cloud.gcp.autoconfigure.logging;
 
 import com.google.cloud.logging.TraceLoggingEnhancer;
 
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -27,7 +28,8 @@ import org.springframework.cloud.gcp.autoconfigure.logging.extractors.TraceIdExt
 import org.springframework.cloud.gcp.autoconfigure.logging.extractors.TraceIdExtractorType;
 import org.springframework.cloud.gcp.autoconfigure.logging.extractors.XCloudTraceIdExtractor;
 import org.springframework.cloud.gcp.autoconfigure.logging.extractors.ZipkinTraceIdExtractor;
-import org.springframework.cloud.gcp.core.GcpProjectIdProvider;
+import org.springframework.cloud.gcp.autoconfigure.trace.StackdriverTraceAutoConfiguration;
+import org.springframework.cloud.sleuth.autoconfig.SleuthProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -38,6 +40,8 @@ import org.springframework.context.annotation.Import;
  */
 @Configuration
 @ConditionalOnClass(TraceLoggingEnhancer.class)
+@ConditionalOnMissingBean(SleuthProperties.class)
+@AutoConfigureAfter(StackdriverTraceAutoConfiguration.class)
 @EnableConfigurationProperties({ StackdriverLoggingProperties.class })
 @ConditionalOnProperty(value = "spring.cloud.gcp.logging.enabled", matchIfMissing = true)
 @Import(LoggingWebMvcConfigurer.class)
@@ -46,9 +50,8 @@ public class StackdriverLoggingAutoConfiguration {
 	@Bean
 	@ConditionalOnMissingBean
 	public TraceIdLoggingWebMvcInterceptor loggingWebMvcInterceptor(
-			TraceIdExtractor extractor,
-			GcpProjectIdProvider projectIdProvider) {
-		return new TraceIdLoggingWebMvcInterceptor(extractor, projectIdProvider);
+			TraceIdExtractor extractor) {
+		return new TraceIdLoggingWebMvcInterceptor(extractor);
 	}
 
 	@Bean

spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/logging/StackdriverTraceConstants.java
@@ -1,0 +1,80 @@
+/*
+ *  Copyright 2018 original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.springframework.cloud.gcp.autoconfigure.logging;
+
+import org.springframework.util.Assert;
+
+/**
+ * @author João André Martins
+ */
+public final class StackdriverTraceConstants {
+
+	private StackdriverTraceConstants() { }
+
+	/**
+	 * The JSON field name for the log level (severity)
+	 */
+	public static final String SEVERITY_ATTRIBUTE = "severity";
+
+	/**
+	 * The JSON field name for the seconds of the timestamp
+	 */
+	public static final String TIMESTAMP_SECONDS_ATTRIBUTE = "timestampSeconds";
+
+	/**
+	 * The JSON field name for the nanos of the timestamp
+	 */
+	public static final String TIMESTAMP_NANOS_ATTRIBUTE = "timestampNanos";
+
+	/**
+	 * The JSON field name for the span-id
+	 */
+	public static final String SPAN_ID_ATTRIBUTE = "logging.googleapis.com/spanId";
+
+	/**
+	 * The JSON field name for the trace-id
+	 */
+	public static final String TRACE_ID_ATTRIBUTE = "logging.googleapis.com/trace";
+
+	/**
+	 * The name of the MDC parameter, Spring Sleuth is storing the trace id at
+	 */
+	public static final String MDC_FIELD_TRACE_ID = "X-B3-TraceId";
+
+	/**
+	 * The name of the MDC parameter, Spring Sleuth is storing the span id at
+	 */
+	public static final String MDC_FIELD_SPAN_ID = "X-B3-SpanId";
+
+	/**
+	 * The name of the MDC parameter, Spring Sleuth is storing the span export information at
+	 */
+	public static final String MDC_FIELD_SPAN_EXPORT = "X-Span-Export";
+
+	/**
+	 * Composes the full trace name as expected by the Google Developers Console log viewer, to
+	 * enable trace correlation with log entries.
+	 * @param projectId The GCP project ID
+	 * @param traceId The trace ID
+	 * @return the trace name in the "projects/[GCP_PROJECT_ID]/trace/[TRACE_ID]" format
+	 */
+	public static String composeFullTraceName(String projectId, String traceId) {
+		Assert.notNull(projectId, "The project ID can't be null.");
+		Assert.notNull(traceId, "The trace ID can't be null.");
+		return "projects/" + projectId + "/traces/" + traceId;
+	}
+}

spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/logging/TraceIdLoggingEnhancer.java
@@ -19,6 +19,9 @@ package org.springframework.cloud.gcp.autoconfigure.logging;
 import com.google.cloud.logging.LogEntry;
 import com.google.cloud.logging.LoggingEnhancer;
 
+import org.springframework.cloud.gcp.core.DefaultGcpProjectIdProvider;
+import org.springframework.cloud.gcp.core.GcpProjectIdProvider;
+
 /**
  * Adds the trace ID to the logging entry, in its correct format to be displayed in the Logs viewer.
  *
@@ -28,28 +31,46 @@ public class TraceIdLoggingEnhancer implements LoggingEnhancer {
 
 	private static final ThreadLocal<String> traceId = new ThreadLocal<>();
 
-	public static void setCurrentTraceId(String projectId, String id) {
+	private GcpProjectIdProvider projectIdProvider = new DefaultGcpProjectIdProvider();
+
+	public static void setCurrentTraceId(String id) {
 		if (id == null) {
 			traceId.remove();
 		}
 		else {
-			traceId.set("projects/" + projectId + "/traces/" + id);
+			traceId.set(id);
 		}
 	}
 
 	/**
-	 * Returns the trace ID in the "projects/[MY_PROJECT_ID]/traces/[MY_TRACE_ID]".
-	 * @return the trace ID in the "projects/[MY_PROJECT_ID]/traces/[MY_TRACE_ID]"
+	 * @return the trace ID stored through {@link #setCurrentTraceId(String)}
 	 */
 	public static String getCurrentTraceId() {
 		return traceId.get();
 	}
 
+	/**
+	 * Set the trace field of the log entry to the current trace ID.
+	 * <p>The current trace ID is either the trace ID stored in the Mapped Diagnostic Context (MDC)
+	 * under the "X-B3-TraceId" key or, if none set, the current trace ID set by
+	 * {@link #setCurrentTraceId(String)}.
+	 * <p>The trace ID is set in the log entry in the "projects/[GCP_PROJECT_ID]/traces/[TRACE_ID]"
+	 * format, in order to be associated to traces by the Google Cloud Console.
+	 * @param builder log entry builder
+	 */
 	@Override
 	public void enhanceLogEntry(LogEntry.Builder builder) {
-		String traceId = getCurrentTraceId();
+		// In order not to duplicate the whole google-cloud-logging-logback LoggingAppender to add
+		// the trace ID from the MDC there, we're doing it here.
+		// This requires a call to the org.slf4j package.
+		String traceId = org.slf4j.MDC.get(StackdriverTraceConstants.MDC_FIELD_TRACE_ID);
+		if (traceId == null) {
+			traceId = getCurrentTraceId();
+		}
+
 		if (traceId != null) {
-			builder.setTrace(traceId);
+			builder.setTrace(StackdriverTraceConstants.composeFullTraceName(
+					this.projectIdProvider.getProjectId(), traceId));
 		}
 	}
 }

spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/logging/TraceIdLoggingWebMvcInterceptor.java
@@ -21,7 +21,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.cloud.gcp.autoconfigure.logging.extractors.TraceIdExtractor;
-import org.springframework.cloud.gcp.core.GcpProjectIdProvider;
 import org.springframework.util.Assert;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -40,14 +39,9 @@ public class TraceIdLoggingWebMvcInterceptor extends HandlerInterceptorAdapter {
 
 	private final TraceIdExtractor traceIdExtractor;
 
-	private final GcpProjectIdProvider projectIdProvider;
-
-	public TraceIdLoggingWebMvcInterceptor(TraceIdExtractor extractor,
-			GcpProjectIdProvider projectIdProvider) {
+	public TraceIdLoggingWebMvcInterceptor(TraceIdExtractor extractor) {
 		Assert.notNull(extractor, "A valid trace id extractor is required.");
-		Assert.notNull(projectIdProvider, "A valid project ID provider is required.");
 		this.traceIdExtractor = extractor;
-		this.projectIdProvider = projectIdProvider;
 	}
 
 	public TraceIdExtractor getTraceIdExtractor() {
@@ -59,8 +53,7 @@ public class TraceIdLoggingWebMvcInterceptor extends HandlerInterceptorAdapter {
 			HttpServletResponse resp, Object handler) throws Exception {
 		String traceId = this.traceIdExtractor.extractTraceIdFromRequest(req);
 		if (traceId != null) {
-			TraceIdLoggingEnhancer.setCurrentTraceId(
-					this.projectIdProvider.getProjectId(), traceId);
+			TraceIdLoggingEnhancer.setCurrentTraceId(traceId);
 		}
 		return true;
 	}
@@ -70,6 +63,6 @@ public class TraceIdLoggingWebMvcInterceptor extends HandlerInterceptorAdapter {
 			HttpServletResponse httpServletResponse, Object handler, Exception e) throws Exception {
 		// Note: the thread-local is currently not fully cleared, but just set to null
 		// See: https://github.com/GoogleCloudPlatform/google-cloud-java/issues/2746
-		TraceIdLoggingEnhancer.setCurrentTraceId(this.projectIdProvider.getProjectId(), null);
+		TraceIdLoggingEnhancer.setCurrentTraceId(null);
 	}
 }

spring-cloud-gcp-autoconfigure/src/test/java/org/springframework/cloud/gcp/autoconfigure/logging/StackdriverJsonLayoutLoggerTests.java
@@ -46,9 +46,9 @@ public class StackdriverJsonLayoutLoggerTests {
 			ByteArrayOutputStream out = new ByteArrayOutputStream();
 			System.setOut(new java.io.PrintStream(out));
 
-			mdc.put(StackdriverJsonLayout.MDC_FIELD_TRACE_ID, "12345678901234561234567890123456");
-			mdc.put(StackdriverJsonLayout.MDC_FIELD_SPAN_ID, "span123");
-			mdc.put(StackdriverJsonLayout.MDC_FIELD_SPAN_EXPORT, "true");
+			mdc.put(StackdriverTraceConstants.MDC_FIELD_TRACE_ID, "12345678901234561234567890123456");
+			mdc.put(StackdriverTraceConstants.MDC_FIELD_SPAN_ID, "span123");
+			mdc.put(StackdriverTraceConstants.MDC_FIELD_SPAN_EXPORT, "true");
 			mdc.put("foo", "bar");
 
 			LOGGER.warn("test");
@@ -56,18 +56,18 @@ public class StackdriverJsonLayoutLoggerTests {
 			Map data = new Gson().fromJson(new String(out.toByteArray()), Map.class);
 
 			checkData(JsonLayout.FORMATTED_MESSAGE_ATTR_NAME, "test", data);
-			checkData(StackdriverJsonLayout.SEVERITY_ATTRIBUTE, "WARN", data);
+			checkData(StackdriverTraceConstants.SEVERITY_ATTRIBUTE, "WARN", data);
 			checkData(StackdriverJsonLayout.LOGGER_ATTR_NAME, "StackdriverJsonLayoutLoggerTests", data);
-			checkData(StackdriverJsonLayout.TRACE_ID_ATTRIBUTE,
+			checkData(StackdriverTraceConstants.TRACE_ID_ATTRIBUTE,
 					"projects/test-project/traces/12345678901234561234567890123456", data);
-			checkData(StackdriverJsonLayout.SPAN_ID_ATTRIBUTE, "span123", data);
+			checkData(StackdriverTraceConstants.SPAN_ID_ATTRIBUTE, "span123", data);
 			checkData("foo", "bar", data);
-			assertFalse(data.containsKey(StackdriverJsonLayout.MDC_FIELD_TRACE_ID));
-			assertFalse(data.containsKey(StackdriverJsonLayout.MDC_FIELD_SPAN_ID));
-			assertFalse(data.containsKey(StackdriverJsonLayout.MDC_FIELD_SPAN_EXPORT));
+			assertFalse(data.containsKey(StackdriverTraceConstants.MDC_FIELD_TRACE_ID));
+			assertFalse(data.containsKey(StackdriverTraceConstants.MDC_FIELD_SPAN_ID));
+			assertFalse(data.containsKey(StackdriverTraceConstants.MDC_FIELD_SPAN_EXPORT));
 			assertFalse(data.containsKey(JsonLayout.TIMESTAMP_ATTR_NAME));
-			assertTrue(data.containsKey(StackdriverJsonLayout.TIMESTAMP_SECONDS_ATTRIBUTE));
-			assertTrue(data.containsKey(StackdriverJsonLayout.TIMESTAMP_NANOS_ATTRIBUTE));
+			assertTrue(data.containsKey(StackdriverTraceConstants.TIMESTAMP_SECONDS_ATTRIBUTE));
+			assertTrue(data.containsKey(StackdriverTraceConstants.TIMESTAMP_NANOS_ATTRIBUTE));
 		}
 		finally {
 			System.setOut(oldOut);
@@ -83,13 +83,13 @@ public class StackdriverJsonLayoutLoggerTests {
 			ByteArrayOutputStream out = new ByteArrayOutputStream();
 			System.setOut(new java.io.PrintStream(out));
 
-			mdc.put(StackdriverJsonLayout.MDC_FIELD_TRACE_ID, "1234567890123456");
+			mdc.put(StackdriverTraceConstants.MDC_FIELD_TRACE_ID, "1234567890123456");
 
 			LOGGER.warn("test");
 
 			Map data = new Gson().fromJson(new String(out.toByteArray()), Map.class);
 
-			checkData(StackdriverJsonLayout.TRACE_ID_ATTRIBUTE,
+			checkData(StackdriverTraceConstants.TRACE_ID_ATTRIBUTE,
 					"projects/test-project/traces/00000000000000001234567890123456", data);
 		}
 		finally {

commit 71585a01d71fa4d044d4ff8a5f09331af0b5da21

Fixes build
Build was broken after #714.

spring-cloud-gcp-autoconfigure/src/test/java/org/springframework/cloud/gcp/autoconfigure/logging/StackdriverLoggingAutoConfigurationTests.java
@@ -18,6 +18,8 @@ package org.springframework.cloud.gcp.autoconfigure.logging;
 
 import java.util.List;
 
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.auth.Credentials;
 import org.junit.Test;
 
 import org.springframework.boot.autoconfigure.AutoConfigurations;
@@ -29,8 +31,10 @@ import org.springframework.cloud.gcp.autoconfigure.logging.extractors.TraceIdExt
 import org.springframework.cloud.gcp.autoconfigure.logging.extractors.XCloudTraceIdExtractor;
 import org.springframework.cloud.gcp.autoconfigure.logging.extractors.ZipkinTraceIdExtractor;
 import org.springframework.cloud.gcp.autoconfigure.trace.StackdriverTraceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 /**
  * @author Mike Eltsufin
@@ -122,10 +126,20 @@ public class StackdriverLoggingAutoConfigurationTests {
 	@Test
 	public void testWithSleuth() {
 		this.contextRunner
-				.withConfiguration(AutoConfigurations.of(StackdriverTraceAutoConfiguration.class))
+				.withConfiguration(AutoConfigurations.of(Configuration.class,
+						StackdriverTraceAutoConfiguration.class))
+				.withPropertyValues("spring.cloud.gcp.project-id=pop-1")
 				.run(context -> {
 					assertThat(context.getBeansOfType(TraceIdLoggingWebMvcInterceptor.class).size())
 							.isEqualTo(0);
 				});
+	}
+
+	private static class Configuration {
+
+		@Bean
+		public CredentialsProvider googleCredentials() {
+			return () -> mock(Credentials.class);
+		}
 	}
 }