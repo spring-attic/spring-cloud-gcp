d10d4f22c1ef20a45ebb47f7e8eed8a79a021aec
Artem Bilan <abilan@pivotal.io>
2018-06-25 13:33:52 -0400

    Update `GcsStreamingMessageSource` metadata

--- spring-cloud-gcp-storage/src/main/java/org/springframework/cloud/gcp/storage/integration/inbound/GcsStreamingMessageSource.java
+++ spring-cloud-gcp-storage/src/main/java/org/springframework/cloud/gcp/storage/integration/inbound/GcsStreamingMessageSource.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2017 original author or authors.
+ *  Copyright 2017-2018 original author or authors.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -31,6 +31,7 @@ import org.springframework.integration.metadata.SimpleMetadataStore;
 
 /**
  * @author João André Martins
+ * @author Mike Eltsufin
  */
 public class GcsStreamingMessageSource extends AbstractRemoteFileStreamingMessageSource<BlobInfo> {
 
