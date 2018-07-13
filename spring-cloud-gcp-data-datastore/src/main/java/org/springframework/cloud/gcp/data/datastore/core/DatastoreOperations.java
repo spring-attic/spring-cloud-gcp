6395acca94174391a615882562fbbc8b5f9f62a5
ChengyuanZhao <cccz@google.com>
2018-07-13 12:58:25 -0400

    simplifying PR

--- spring-cloud-gcp-data-datastore/src/main/java/org/springframework/cloud/gcp/data/datastore/core/DatastoreOperations.java
+++ spring-cloud-gcp-data-datastore/src/main/java/org/springframework/cloud/gcp/data/datastore/core/DatastoreOperations.java
@@ -34,81 +34,12 @@ import org.springframework.lang.Nullable;
 public interface DatastoreOperations {
 
 	/**
-	 * Get an entity based on a key.
+	 * Get an entity based on a id.
 	 * @param entityClass the type of the entity to get.
-	 * @param key the key of the entity
+	 * @param id the id of the entity
 	 * @param <T> the class type of the entity.
-	 * @param readOptions optional read options.
-	 * @return the entity that was found with that key.
+	 * @return the entity that was found with that id.
 	 */
-	<T> T read(Class<T> entityClass, Key key, @Nullable DatastoreReadOptions readOptions);
+	<T> T findById(Class<T> entityClass, Object id);
 
-	/**
-	 * Get a list of entities based on a list of keys.
-	 * @param entityClass the class type of the entities to get.
-	 * @param keys the keys of the entities to get.
-	 * @param <T> the type of the class
-	 * @param readOptions optional read options.
-	 * @return the list of entities that were found.
-	 */
-	<T> List<T> read(Class<T> entityClass, Iterable<Key> keys,
-			@Nullable DatastoreReadOptions readOptions);
-
-	/**
-	 * Get a list of all entities of the given class type.
-	 * @param entityClass the class type of entity to retrieve.
-	 * @param <T> the type of the entity.
-	 * @param readOptions optional read options.
-	 * @return a list of entities of the given class type.
-	 */
-	<T> List<T> readAll(Class<T> entityClass, @Nullable DatastoreReadOptions readOptions);
-
-	/**
-	 * Get a list of entities using a query.
-	 * @param entityClass the type of the entity to retrieve.
-	 * @param query the query to run
-	 * @param readOptions optional read options.
-	 * @param <T> the class type of the entity.
-	 * @return a list of entities found.
-	 */
-	<T> List<T> query(Class<T> entityClass, Query<Entity> query,
-			@Nullable DatastoreReadOptions readOptions);
-
-	/**
-	 * Delete an entity from Cloud Datastore based on a key.
-	 * @param key the key of the entity to delete.
-	 */
-	void delete(Key key);
-
-	/**
-	 * Delete multiple entities from Cloud Datastore based on their keys.
-	 * @param keys the keys of the entities to delete.
-	 */
-	void delete(Key... keys);
-
-	/**
-	 * Delete an entity from Cloud Datastore.
-	 * @param entity the object that will be deleted based on its key value.
-	 */
-	void delete(Object entity);
-
-	/**
-	 * Update an object to Cloud Datastore. Will fail if an entity with the same key
-	 * column value doesn't exist in Cloud Datastore.
-	 * @param entity the object to update.
-	 */
-	void update(Object entity);
-
-	/**
-	 * Update or insert an object to Cloud Datastore.
-	 * @param entity the object to update or insert.
-	 */
-	void put(Object entity);
-
-	/**
-	 * Count the number of entities of the given class in Cloud Datastore.
-	 * @param entityClass the class type to count.
-	 * @return the number of entities.
-	 */
-	<T> long count(Class<T> entityClass);
 }
