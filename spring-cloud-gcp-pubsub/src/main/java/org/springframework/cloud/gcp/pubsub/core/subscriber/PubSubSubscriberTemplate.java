aa17c12c13823e27fd2e26edee40a6bc088b5a7a
Mike Eltsufin <meltsufin@google.com>
2018-09-11 13:43:10 -0400

    Fix pull-and-ack on empty list
    Stop acking the list of messages, if the pull resulted in an empty list.
    
    Fixes #1009.

--- spring-cloud-gcp-pubsub/src/main/java/org/springframework/cloud/gcp/pubsub/core/subscriber/PubSubSubscriberTemplate.java
+++ spring-cloud-gcp-pubsub/src/main/java/org/springframework/cloud/gcp/pubsub/core/subscriber/PubSubSubscriberTemplate.java
@@ -194,7 +194,8 @@ public class PubSubSubscriberTemplate implements PubSubSubscriberOperations {
 
 		List<AcknowledgeablePubsubMessage> ackableMessages = pull(pullRequest);
 
-		ack(ackableMessages);
+		if (ackableMessages.size() > 0)
+			ack(ackableMessages);
 
 		return ackableMessages.stream().map(AcknowledgeablePubsubMessage::getPubsubMessage)
 				.collect(Collectors.toList());
