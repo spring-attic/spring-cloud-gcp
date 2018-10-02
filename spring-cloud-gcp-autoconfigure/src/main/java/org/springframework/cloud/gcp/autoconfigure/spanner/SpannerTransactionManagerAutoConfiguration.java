bf17704108b7a6264e5e8503d11f000f785f0ea9
ChengyuanZhao <cccz@google.com>
2018-10-02 14:41:15 -0400

    removed property to enable trmgr

--- spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/spanner/SpannerTransactionManagerAutoConfiguration.java
+++ spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/spanner/SpannerTransactionManagerAutoConfiguration.java
@@ -22,7 +22,6 @@ import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
 import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
@@ -42,8 +41,6 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 @ConditionalOnClass(SpannerTransactionManager.class)
 @AutoConfigureBefore(TransactionAutoConfiguration.class)
-@ConditionalOnProperty(value = "spring.cloud.gcp.spanner.transactional-annotation.enabled",
-		matchIfMissing = true)
 @EnableConfigurationProperties(GcpSpannerProperties.class)
 public class SpannerTransactionManagerAutoConfiguration {
 
