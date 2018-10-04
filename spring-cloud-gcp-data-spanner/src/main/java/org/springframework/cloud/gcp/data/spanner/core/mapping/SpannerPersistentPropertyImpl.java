ed08ec39816576adafc3a8e0c1a0b72b67513993
ChengyuanZhao <cccz@google.com>
2018-10-04 14:28:37 -0400

    typo fix

--- spring-cloud-gcp-data-spanner/src/main/java/org/springframework/cloud/gcp/data/spanner/core/mapping/SpannerPersistentPropertyImpl.java
+++ spring-cloud-gcp-data-spanner/src/main/java/org/springframework/cloud/gcp/data/spanner/core/mapping/SpannerPersistentPropertyImpl.java
@@ -166,7 +166,7 @@ public class SpannerPersistentPropertyImpl
 	@Override
 	public boolean isGenerateSchemaNotNull() {
 		Column annotation = findAnnotation(Column.class);
-		return annotation != null && annotation.nullable();
+		return annotation != null && !annotation.nullable();
 	}
 
 	@Override
