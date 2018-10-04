193ca2c934831bc74596ba9ae950f3f96e94a1b4
dzou <dzou@google.com>
2018-10-04 19:11:04 -0400

    Move spanner samples under 1 main method + flag and improve tests.

---
+++ spring-cloud-gcp-samples/spring-cloud-gcp-data-spanner-sample/src/main/java/com/example/SpannerExampleDriver.java
@@ -0,0 +1,52 @@
+package com.example;
+
+import java.util.Arrays;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+public class SpannerExampleDriver {
+
+	private static final Log LOGGER = LogFactory.getLog(SpannerExampleDriver.class);
+
+	@Autowired
+	private SpannerRepositoryExample spannerRepositoryExample;
+
+	@Autowired
+	private SpannerTemplateExample spannerTemplateExample;
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(args));
+		SpringApplication.run(SpannerExampleDriver.class, args);
+	}
+
+	@Bean
+	@Profile("!test")
+	ApplicationRunner applicationRunner() {
+		return args -> {
+			if (!args.containsOption("spanner_repository") && !args.containsOption("spanner_template")) {
+				throw new IllegalArgumentException("To run the Spanner example, please specify "
+						+ " -Dspring-boot.run.arguments=--spanner_repository to run the Spanner repository"
+						+ " example or -Dspring-boot.run.arguments=--spanner_template to"
+						+ " run the Spanner template example.");
+			}
+
+			if (args.containsOption("spanner_repository")) {
+				LOGGER.info("Running the Spanner Repository Example.");
+				this.spannerRepositoryExample.runExample();
+			}
+			else if (args.containsOption("spanner_template")) {
+				LOGGER.info("Running the Spanner Template Example.");
+				this.spannerTemplateExample.runExample();
+			}
+		};
+	}
+}
--- spring-cloud-gcp-samples/spring-cloud-gcp-data-spanner-sample/src/test/java/com/example/SpannerRepositoryTests.java
+++ spring-cloud-gcp-samples/spring-cloud-gcp-data-spanner-sample/src/test/java/com/example/SpannerRepositoryTests.java
@@ -1,27 +1,41 @@
 package com.example;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assume.assumeThat;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import com.google.common.collect.ImmutableList;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.cloud.gcp.data.spanner.core.admin.SpannerDatabaseAdminTemplate;
 import org.springframework.cloud.gcp.data.spanner.core.admin.SpannerSchemaUtils;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assume.assumeThat;
+
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 @TestPropertySource("classpath:application-test.properties")
-@SpringBootTest(classes = {SpannerRepositoryExample.class})
-@ActiveProfiles(profiles = "repository-example")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = { SpannerExampleDriver.class })
 public class SpannerRepositoryTests {
+	@LocalServerPort
+	private int port;
 
 	@Autowired
 	private TraderRepository traderRepository;
@@ -36,7 +50,7 @@ public class SpannerRepositoryTests {
 	private SpannerDatabaseAdminTemplate spannerDatabaseAdminTemplate;
 
 	@Autowired
-	private CommandLineRunner commandLineRunner;
+	private SpannerRepositoryExample spannerRepositoryExample;
 
 	@BeforeClass
 	public static void checkToRun() {
@@ -46,15 +60,54 @@ public class SpannerRepositoryTests {
 				System.getProperty("it.spanner"), is("true"));
 	}
 
+	@Before
 	@After
-	public void cleanupSpannerTables() {
+	public void cleanupAndSetupTables() {
+		spannerRepositoryExample.createTablesIfNotExists();
 		this.tradeRepository.deleteAll();
 		this.traderRepository.deleteAll();
 	}
 
 	@Test
-	public void verifySpannerApplicationLoadsRows() {
-		// Verifies that there are 3 items in the Trader Repository table,
-		assertThat(traderRepository.count()).isEqualTo(3);
+	public void testRestEndpoint() {
+		spannerRepositoryExample.runExample();
+
+		TestRestTemplate testRestTemplate = new TestRestTemplate();
+		ResponseEntity<PagedResources<Trade>> tradesResponse = testRestTemplate.exchange(
+				String.format("http://localhost:%s/trades/", port),
+				HttpMethod.GET,
+				null,
+				new ParameterizedTypeReference<PagedResources<Trade>>() {
+				});
+		assertThat(tradesResponse.getBody().getMetadata().getTotalElements()).isEqualTo(8);
+	}
+
+	@Test
+	public void testLoadsCorrectData() {
+		assertThat(traderRepository.count()).isEqualTo(0);
+		assertThat(tradeRepository.count()).isEqualTo(0);
+
+		spannerRepositoryExample.runExample();
+		List<String> traderIds = ImmutableList.copyOf(traderRepository.findAll())
+				.stream()
+				.map(Trader::getTraderId)
+				.collect(Collectors.toList());
+		assertThat(traderIds).containsExactlyInAnyOrder("demo_trader1", "demo_trader2", "demo_trader3");
+
+		List<Trade> actualTrades = ImmutableList.copyOf(tradeRepository.findAll());
+		assertThat(actualTrades).hasSize(8);
+
+		Set<String> tradeSpannerKeys = actualTrades.stream()
+				.map(t -> spannerSchemaUtils.getKey(t).toString())
+				.collect(Collectors.toSet());
+		assertThat(tradeSpannerKeys).containsExactlyInAnyOrder(
+				"[demo_trader1,1]",
+				"[demo_trader1,2]",
+				"[demo_trader1,3]",
+				"[demo_trader2,1]",
+				"[demo_trader2,2]",
+				"[demo_trader2,3]",
+				"[demo_trader3,1]",
+				"[demo_trader3,2]");
 	}
 }
