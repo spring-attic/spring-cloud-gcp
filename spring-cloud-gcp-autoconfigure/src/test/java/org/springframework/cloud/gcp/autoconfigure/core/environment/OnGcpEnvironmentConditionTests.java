1902c69caa7a31fc21938f74b43a69a4b13147d3
Elena Felder <41136058+elefeint@users.noreply.github.com>
2019-01-02 14:19:21 -0500

    Remove Guava from core and autoconfig (#1338)
    Conversion followed a few patterns:
    * List literals use `Arrays.asList()`.
    * Single-element maps use `Collections.singletonMap()`.
    * Multi-element maps use the new `MapBuilder`.

--- spring-cloud-gcp-autoconfigure/src/test/java/org/springframework/cloud/gcp/autoconfigure/core/environment/OnGcpEnvironmentConditionTests.java
+++ spring-cloud-gcp-autoconfigure/src/test/java/org/springframework/cloud/gcp/autoconfigure/core/environment/OnGcpEnvironmentConditionTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 the original author or authors.
+ * Copyright 2017-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,8 @@
 
 package org.springframework.cloud.gcp.autoconfigure.core.environment;
 
-import com.google.common.collect.ImmutableMap;
+import java.util.Collections;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -147,7 +148,7 @@ public class OnGcpEnvironmentConditionTests {
 
 	private void setUpAnnotationValue(Object environments) {
 		when(this.mockMetadata.getAnnotationAttributes(ConditionalOnGcpEnvironment.class.getName())).thenReturn(
-				ImmutableMap.of("value", environments)
+				Collections.singletonMap("value", environments)
 		);
 	}
 }
