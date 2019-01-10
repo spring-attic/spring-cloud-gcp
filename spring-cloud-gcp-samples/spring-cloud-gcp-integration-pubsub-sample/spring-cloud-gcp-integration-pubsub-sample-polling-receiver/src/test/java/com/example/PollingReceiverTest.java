99d3d902630e017ddb47d6c327b0b2de1bc1d41e
Elena Felder <41136058+elefeint@users.noreply.github.com>
2019-01-10 10:27:18 -0500

    remove manual thread sleep

--- spring-cloud-gcp-samples/spring-cloud-gcp-integration-pubsub-sample/spring-cloud-gcp-integration-pubsub-sample-polling-receiver/src/test/java/com/example/PollingReceiverTest.java
+++ spring-cloud-gcp-samples/spring-cloud-gcp-integration-pubsub-sample/spring-cloud-gcp-integration-pubsub-sample-polling-receiver/src/test/java/com/example/PollingReceiverTest.java
@@ -19,8 +19,10 @@ package com.example;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.output.TeeOutputStream;
+import org.awaitility.Awaitility;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -76,17 +78,13 @@ public class PollingReceiverTest {
 	@Test
 	public void testSample() throws Exception {
 		String message = "test message " + UUID.randomUUID();
+		String expectedString = "Message arrived by Synchronous Pull! Payload: " + message;
 
 		this.pubSubTemplate.publish("exampleTopic", message);
 
-		boolean messageReceived = false;
-		for (int i = 0; i < 100; i++) {
-			if (baos.toString().contains("Message arrived by Synchronous Pull! Payload: " + message)) {
-				messageReceived = true;
-				break;
-			}
-			Thread.sleep(100);
-		}
-		assertThat(messageReceived).isTrue();
+		Awaitility.await()
+				.atMost(10, TimeUnit.SECONDS)
+				.until(() -> baos.toString().contains(expectedString));
+		assertThat(baos.toString()).contains(expectedString);
 	}
 }
