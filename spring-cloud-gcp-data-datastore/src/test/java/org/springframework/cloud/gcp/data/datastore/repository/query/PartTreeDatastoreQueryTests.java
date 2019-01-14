a2d6f28854a128860394d889041317c641a92081
Elena Felder <41136058+elefeint@users.noreply.github.com>
2019-01-14 12:09:28 -0500

    remove streams, replace with simple list population

--- spring-cloud-gcp-data-datastore/src/test/java/org/springframework/cloud/gcp/data/datastore/repository/query/PartTreeDatastoreQueryTests.java
+++ spring-cloud-gcp-data-datastore/src/test/java/org/springframework/cloud/gcp/data/datastore/repository/query/PartTreeDatastoreQueryTests.java
@@ -22,11 +22,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Spliterator;
-import java.util.Spliterators;
 import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 import com.google.cloud.datastore.EntityQuery;
 import com.google.cloud.datastore.KeyQuery;
@@ -536,9 +532,11 @@ public class PartTreeDatastoreQueryTests {
 					: results;
 		});
 		when(this.datastoreTemplate.convertEntitiesForRead(any(), any())).then(
-				(invocation) -> StreamSupport.stream(Spliterators.spliteratorUnknownSize(
-						invocation.<Iterator>getArgument(0), Spliterator.ORDERED), false)
-						.collect(Collectors.toList())
+				(invocation) -> {
+					List<Object> list = new ArrayList<>();
+					invocation.<Iterator>getArgument(0).forEachRemaining(list::add);
+					return list;
+				}
 		);
 	}
 
