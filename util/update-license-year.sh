5086c1c79d6653b5b4709500c7e870cebd2ffd05
Elena Felder <41136058+elefeint@users.noreply.github.com>
2019-01-15 16:29:36 -0500

    update license script

--- util/update-license-year.sh
+++ util/update-license-year.sh
@@ -6,6 +6,6 @@
 cur_year=$(date +'%Y');
 echo "Updating year to ${cur_year}"
 
-git diff --name-only HEAD -- '*.java' | \
-  xargs grep --files-without-match "^ \* Copyright ....-$cur_year" | \
-  xargs sed -i -e "s/^\( \* Copyright ....\)-..../\1-$cur_year/"
+git diff --name-only --diff-filter=ACMR HEAD -- '*.java' | \
+  xargs --no-run-if-empty grep --files-without-match "^ \* Copyright ....-$cur_year" | \
+  xargs --no-run-if-empty sed -i -e "s/^\( \* Copyright ....\)-..../\1-$cur_year/"
