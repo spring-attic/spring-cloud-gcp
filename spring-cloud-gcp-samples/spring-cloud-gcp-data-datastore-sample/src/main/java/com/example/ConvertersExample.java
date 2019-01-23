006b13ac995145ea90c676fa8f816db8275bdf29
Elena Felder <41136058+elefeint@users.noreply.github.com>
2019-01-23 16:01:04 -0500

    Remove remaining guava presence (#1381)
    Last part of Guava removal. 
    
    * `ImmutableMap` turned into `Collections.singletonMap()` for a single pair or into `MapBuilder` for multiple pairs.
    * `ImmutableSet` turned into a new set created based on a list.
    * `ImmutableList` turned into `Arrays.asList()`.
    * `ConvertersExample.LIST_IMMUTABLE_SET_CONVERTER` no longer needed because `Set` and `List` are automatically convertable. The custom object converters in the same class are illustrate the concept well already, so no need to replace with a more convoluted data structure.
    
    Fixes #1198 .

--- spring-cloud-gcp-samples/spring-cloud-gcp-data-datastore-sample/src/main/java/com/example/ConvertersExample.java
+++ spring-cloud-gcp-samples/spring-cloud-gcp-data-datastore-sample/src/main/java/com/example/ConvertersExample.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 the original author or authors.
+ * Copyright 2017-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,9 +18,6 @@ package com.example;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-
-import com.google.common.collect.ImmutableSet;
 
 import org.springframework.core.convert.converter.Converter;
 
@@ -30,15 +27,6 @@ import org.springframework.core.convert.converter.Converter;
  * @author Dmitry Solomakha
  */
 public class ConvertersExample {
-	//Converter to read ImmutableSet
-	//Note that you don't need a ImmutableSet to List converter
-	static final Converter<List<?>, ImmutableSet<?>> LIST_IMMUTABLE_SET_CONVERTER =
-			new Converter<List<?>, ImmutableSet<?>>() {
-				@Override
-				public ImmutableSet<?> convert(List<?> source) {
-					return ImmutableSet.copyOf(source);
-				}
-			};
 
 	//Converter to write custom Singer.Album type
 	static final Converter<Album, String> ALBUM_STRING_CONVERTER =
