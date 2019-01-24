bc8c63ad35509e9e23ee620c32487e788d02529d
dmitry-s <dsolomakha@google.com>
2019-01-24 11:20:27 -0500

    Update spring-cloud-gcp-data-spanner/src/main/java/org/springframework/cloud/gcp/data/spanner/core/mapping/event/BeforeExecuteDmlEvent.java
    Co-Authored-By: ChengyuanZhao <cccz@google.com>

--- spring-cloud-gcp-data-spanner/src/main/java/org/springframework/cloud/gcp/data/spanner/core/mapping/event/BeforeExecuteDmlEvent.java
+++ spring-cloud-gcp-data-spanner/src/main/java/org/springframework/cloud/gcp/data/spanner/core/mapping/event/BeforeExecuteDmlEvent.java
@@ -27,7 +27,7 @@ public class BeforeExecuteDmlEvent extends ExecuteDmlEvent {
 	/**
 	 * Constructor.
 	 *
-	 * @param statement the DML statement which is (never {@code null})
+	 * @param statement the DML statement which is never {@code null}
 	 */
 	public BeforeExecuteDmlEvent(Statement statement) {
 		super(statement);
