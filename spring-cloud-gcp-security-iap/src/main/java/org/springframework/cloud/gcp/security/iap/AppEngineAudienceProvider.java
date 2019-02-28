6a5c30713cfd69ff21781a43a68cdff87bb4ce55
Elena Felder <41136058+elefeint@users.noreply.github.com>
2019-02-28 12:35:57 -0500

    update docs and error message

--- spring-cloud-gcp-security-iap/src/main/java/org/springframework/cloud/gcp/security/iap/AppEngineAudienceProvider.java
+++ spring-cloud-gcp-security-iap/src/main/java/org/springframework/cloud/gcp/security/iap/AppEngineAudienceProvider.java
@@ -48,7 +48,7 @@ public class AppEngineAudienceProvider implements AudienceProvider {
 	@Override
 	public String getAudience() {
 		Project project = this.resourceManager.get(this.projectIdProvider.getProjectId());
-		Assert.notNull(project, "Project expected not to be null.");
+		Assert.notNull(project, "Project expected not to be null. Is Cloud Resource Manager API enabled?");
 		Assert.notNull(project.getProjectNumber(), "Project Number expected not to be null.");
 
 		String projectId = this.projectIdProvider.getProjectId();
