6a56fa11efcd72d99ae28b1502b0b7a325545056
Elena Felder <41136058+elefeint@users.noreply.github.com>
2019-03-21 16:58:18 -0400

    add page for sending messages instead of pointing user to gcloud

--- spring-cloud-gcp-samples/spring-cloud-gcp-pubsub-reactive-sample/src/main/java/com/example/ReactiveReceiverApplication.java
+++ spring-cloud-gcp-samples/spring-cloud-gcp-pubsub-reactive-sample/src/main/java/com/example/ReactiveReceiverApplication.java
@@ -16,8 +16,18 @@
 
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 /**
  * Spring Boot Application demonstrating reactive subscription to PubSub.
@@ -31,6 +41,12 @@ public class ReactiveReceiverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveReceiverApplication.class, args);
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> indexRouter(@Value("classpath:/static/index.html") final Resource indexHtml) {
+		// Serve static index.html at root.
+		return route(GET("/"), request -> ok().contentType(MediaType.TEXT_HTML).syncBody(indexHtml));
 	}
 
 }
