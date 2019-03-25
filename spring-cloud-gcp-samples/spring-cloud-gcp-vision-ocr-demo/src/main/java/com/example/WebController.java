75b2b0d94b45ab1aa7e632421f7df18c71f9cb66
dzou <dzou@users.noreply.github.com>
2019-03-25 17:13:24 -0400

    go back to to just copy/pasting the code snippet

--- spring-cloud-gcp-samples/spring-cloud-gcp-vision-ocr-demo/src/main/java/com/example/WebController.java
+++ spring-cloud-gcp-samples/spring-cloud-gcp-vision-ocr-demo/src/main/java/com/example/WebController.java
@@ -98,7 +98,6 @@ public class WebController {
 		return new ModelAndView("viewDocument", map);
 	}
 
-	// tag::submit_document[]
 	@PostMapping("/submitDocument")
 	public ModelAndView submitDocument(@RequestParam("documentUrl") String documentUrl) throws IOException {
 
@@ -128,7 +127,6 @@ public class WebController {
 
 		return new ModelAndView("submit_done");
 	}
-	// end::submit_document[]
 
 	private static String getFileType(Resource documentResource) {
 		int extensionIdx = documentResource.getFilename().lastIndexOf(".");
