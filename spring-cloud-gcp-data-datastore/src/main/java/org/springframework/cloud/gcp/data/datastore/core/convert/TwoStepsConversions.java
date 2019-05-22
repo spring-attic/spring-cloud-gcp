f00515084af6e1f855edc0b20697c445e24d2da4
dsolomakha <dsolomakha@google.com>
2019-05-22 16:21:18 -0400

    fix map key conversion

--- spring-cloud-gcp-data-datastore/src/main/java/org/springframework/cloud/gcp/data/datastore/core/convert/TwoStepsConversions.java
+++ spring-cloud-gcp-data-datastore/src/main/java/org/springframework/cloud/gcp/data/datastore/core/convert/TwoStepsConversions.java
@@ -290,7 +290,7 @@ public class TwoStepsConversions implements ReadWriteConversions {
 		return applyEntityValueBuilder(kindName, (builder) -> {
 			Map map = (Map) val;
 			for (Object key : map.keySet()) {
-				String field = convertOnReadSingle(key,
+				String field = convertOnReadSingle(convertOnWriteSingle(key).get(),
 						ClassTypeInformation.from(String.class));
 				builder.set(field,
 						convertOnWrite(map.get(key),
