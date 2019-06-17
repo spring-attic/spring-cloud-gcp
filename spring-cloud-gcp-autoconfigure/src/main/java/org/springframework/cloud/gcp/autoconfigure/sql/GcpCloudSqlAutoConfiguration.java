bd0af622e0f03afedde1fe213ec1d3b866e2bffa
Mike Eltsufin <meltsufin@google.com>
2019-06-17 16:59:43 -0400

    Upgrade all dependencies (#1702)

--- spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/sql/GcpCloudSqlAutoConfiguration.java
+++ spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/sql/GcpCloudSqlAutoConfiguration.java
@@ -22,7 +22,7 @@ import java.io.IOException;
 import javax.sql.DataSource;
 
 import com.google.cloud.sql.CredentialFactory;
-import com.google.cloud.sql.core.SslSocketFactory;
+import com.google.cloud.sql.core.CoreSocketFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -199,9 +199,8 @@ public abstract class GcpCloudSqlAutoConfiguration { //NOSONAR squid:S1610 must 
 				setCredentialsFileProperty(gcpCloudSqlProperties, gcpProperties);
 			}
 
-			System.setProperty(SslSocketFactory.USER_TOKEN_PROPERTY_NAME,
-					"spring-cloud-gcp-sql/"
-							+ this.getClass().getPackage().getImplementationVersion());
+			CoreSocketFactory.setApplicationName("spring-cloud-gcp-sql/"
+					+ this.getClass().getPackage().getImplementationVersion());
 
 			return properties;
 		}
