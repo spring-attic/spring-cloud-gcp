5f9bca6ca168382141d633237bb3518c8b075029
Mike Eltsufin <meltsufin@google.com>
2019-07-23 13:46:52 -0400

    Increase timeout for Trace sample test (#1760)

--- spring-cloud-gcp-samples/spring-cloud-gcp-trace-sample/src/test/java/com/example/TraceSampleApplicationTests.java
+++ spring-cloud-gcp-samples/spring-cloud-gcp-trace-sample/src/test/java/com/example/TraceSampleApplicationTests.java
@@ -131,7 +131,7 @@ public class TraceSampleApplicationTests {
 		String logFilter = String.format(
 				"trace=projects/%s/traces/%s", this.projectIdProvider.getProjectId(), uuidString);
 
-		await().atMost(60, TimeUnit.SECONDS)
+		await().atMost(120, TimeUnit.SECONDS)
 				.pollInterval(Duration.TWO_SECONDS)
 				.ignoreExceptions()
 				.untilAsserted(() -> {
