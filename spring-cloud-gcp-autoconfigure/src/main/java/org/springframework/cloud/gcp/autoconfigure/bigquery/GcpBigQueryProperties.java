f13b8bae48ced194849912156766e4e6629d1c3a
dzou <dzou@users.noreply.github.com>
2019-07-24 16:51:04 -0400

    Add bigquery autoconfiguration

---
+++ spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/bigquery/GcpBigQueryProperties.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.gcp.autoconfigure.bigquery;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.cloud.gcp.core.Credentials;
+import org.springframework.cloud.gcp.core.CredentialsSupplier;
+import org.springframework.cloud.gcp.core.GcpScope;
+
+/**
+ * Properties for configuring BigQuery.
+ *
+ * @author Daniel Zou
+ */
+@ConfigurationProperties("spring.cloud.gcp.bigquery")
+public class GcpBigQueryProperties implements CredentialsSupplier {
+
+	/** Overrides the GCP OAuth2 credentials specified in the Core module. */
+	@NestedConfigurationProperty
+	private final Credentials credentials = new Credentials(GcpScope.BIG_QUERY.getUrl());
+
+	private String projectId;
+
+	public Credentials getCredentials() {
+		return this.credentials;
+	}
+
+	public String getProjectId() {
+		return this.projectId;
+	}
+
+	public void setProjectId(String projectId) {
+		this.projectId = projectId;
+	}
+}
