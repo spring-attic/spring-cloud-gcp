ce659783d10986d724fad2e4cf9a88662fa51b3b
cccz <cccz@google.com>
2019-07-24 14:40:16 -0400

    checkstyle

--- spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/datastore/DatastoreNamespaceProvider.java
+++ spring-cloud-gcp-autoconfigure/src/main/java/org/springframework/cloud/gcp/autoconfigure/datastore/DatastoreNamespaceProvider.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.cloud.gcp.autoconfigure.datastore;
 
 import java.util.function.Supplier;
