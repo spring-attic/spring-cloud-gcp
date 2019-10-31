918736d58036aae4272b894da630ee6e0d97958c
dsolomakha <dsolomakha@google.com>
2019-10-31 14:08:56 -0400

    test rollback

--- spring-cloud-gcp-data-firestore/src/test/java/org/springframework/cloud/gcp/data/firestore/transaction/ReactiveFirestoreTransactionManagerTest.java
+++ spring-cloud-gcp-data-firestore/src/test/java/org/springframework/cloud/gcp/data/firestore/transaction/ReactiveFirestoreTransactionManagerTest.java
@@ -24,16 +24,18 @@ import com.google.firestore.v1.CommitResponse;
 import com.google.firestore.v1.Document;
 import com.google.firestore.v1.FirestoreGrpc;
 import com.google.firestore.v1.GetDocumentRequest;
+import com.google.firestore.v1.RollbackRequest;
 import com.google.protobuf.ByteString;
+import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
 import org.junit.Test;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import org.springframework.cloud.gcp.data.firestore.FirestoreDataException;
 import org.springframework.cloud.gcp.data.firestore.FirestoreTemplate;
 import org.springframework.cloud.gcp.data.firestore.FirestoreTemplateTests;
 import org.springframework.transaction.reactive.TransactionalOperator;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -55,7 +57,7 @@ public class ReactiveFirestoreTransactionManagerTest {
 		FirestoreTemplate template = getFirestoreTemplate();
 
 		ReactiveFirestoreTransactionManager txManager = new ReactiveFirestoreTransactionManager(this.firestoreStub, this.parent);
-		TransactionalOperator operator = TransactionalOperator.create(txManager, new DefaultTransactionDefinition());
+		TransactionalOperator operator = TransactionalOperator.create(txManager);
 
 		template.findById(Mono.just("e1"), FirestoreTemplateTests.TestEntity.class)
 				.concatWith(template.findById(Mono.just("e2"), FirestoreTemplateTests.TestEntity.class))
@@ -84,12 +86,34 @@ public class ReactiveFirestoreTransactionManagerTest {
 	}
 
 	@Test
+	public void triggerRollbackCorrectly() {
+
+		FirestoreTemplate template = getFirestoreTemplate();
+
+		ReactiveFirestoreTransactionManager txManager = new ReactiveFirestoreTransactionManager(this.firestoreStub, this.parent);
+		TransactionalOperator operator = TransactionalOperator.create(txManager);
+
+		template.findById(Mono.defer(() -> {throw new FirestoreDataException("BOOM!");}), FirestoreTemplateTests.TestEntity.class)
+				.as(operator::transactional)
+				.as(StepVerifier::create)
+				.expectError()
+				.verify();
+
+
+		verify(this.firestoreStub, times(1)).beginTransaction(any(), any());
+		verify(this.firestoreStub, times(0)).commit(any(), any());
+
+
+		verify(this.firestoreStub, times(1)).rollback(any(), any());
+	}
+
+	@Test
 	public void writeTransaction() {
 
 		FirestoreTemplate template = getFirestoreTemplate();
 
 		ReactiveFirestoreTransactionManager txManager = new ReactiveFirestoreTransactionManager(this.firestoreStub, this.parent);
-		TransactionalOperator operator = TransactionalOperator.create(txManager, new DefaultTransactionDefinition());
+		TransactionalOperator operator = TransactionalOperator.create(txManager);
 
 		doAnswer(invocation -> {
 			CommitRequest commitRequest = invocation.getArgument(0);
@@ -146,6 +170,16 @@ public class ReactiveFirestoreTransactionManagerTest {
 			streamObserver.onCompleted();
 			return null;
 		}).when(this.firestoreStub).commit(any(), any());
+
+		doAnswer(invocation -> {
+			RollbackRequest rollbackRequest = invocation.getArgument(0);
+			StreamObserver<Empty> streamObserver = invocation.getArgument(1);
+
+			assertThat(rollbackRequest.getTransaction()).isEqualTo(ByteString.copyFromUtf8("transaction1"));
+
+			streamObserver.onCompleted();
+			return null;
+		}).when(this.firestoreStub).rollback(any(), any());
 
 
 		doAnswer(invocation -> {
