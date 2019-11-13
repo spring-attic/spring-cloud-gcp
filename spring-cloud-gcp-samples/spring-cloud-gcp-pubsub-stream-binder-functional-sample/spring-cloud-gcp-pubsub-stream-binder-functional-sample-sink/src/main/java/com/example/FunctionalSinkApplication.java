50c37094e9879d3e29d89e14b5414d5383d23fc8
Tianzi Cai <tianzih@outlook.com>
2019-11-13 06:24:45 -0800

    keep sink app from exiting (#2029)

--- spring-cloud-gcp-samples/spring-cloud-gcp-pubsub-stream-binder-functional-sample/spring-cloud-gcp-pubsub-stream-binder-functional-sample-sink/src/main/java/com/example/FunctionalSinkApplication.java
+++ spring-cloud-gcp-samples/spring-cloud-gcp-pubsub-stream-binder-functional-sample/spring-cloud-gcp-pubsub-stream-binder-functional-sample-sink/src/main/java/com/example/FunctionalSinkApplication.java
@@ -29,8 +29,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FunctionalSinkApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(FunctionalSinkApplication.class, args);
+		System.out.println("Hit 'Enter' to terminate");
+		System.in.read();
 	}
 
 }
