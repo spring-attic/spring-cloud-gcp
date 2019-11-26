833860753f4cf6b60012119c4285e31ea2e31584
dsolomakha <dsolomakha@google.com>
2019-11-26 11:33:02 -0500

    make FirestoreClassMapper a bean

--- spring-cloud-gcp-data-firestore/src/main/java/org/springframework/cloud/gcp/data/firestore/mapping/FirestoreClassMapper.java
+++ spring-cloud-gcp-data-firestore/src/main/java/org/springframework/cloud/gcp/data/firestore/mapping/FirestoreClassMapper.java
@@ -22,12 +22,36 @@ import com.google.firestore.v1.Document;
 import com.google.firestore.v1.Value;
 
 /**
+ * An interface used for object mapping for Cloud Firestore.
+ *
  * @author Dmitry Solomakha
  */
 public interface FirestoreClassMapper {
+	/**
+	 * Converts an entity to a Firestore type.
+	 *
+	 * @param <T> the type of the object to convert
+	 * @param entity the object to convert
+	 * @return value that can be saved in Firestore
+	 */
 	<T> Value convertToFirestoreValue(T entity);
 
+	/**
+	 * Converts an entity to a map where key is the property name and the value is a Firestore type.
+	 *
+	 * @param <T> the type of the object to convert
+	 * @param entity the object to convert
+	 * @return a map where key is the property name and the value is a Firestore type
+	 */
 	<T> Map<String, Value> convertToFirestoreTypes(T entity);
 
+	/**
+	 * Converts a Firestore document to a Java object.
+	 *
+	 * @param <T> the type of the target object
+	 * @param document the document to convert
+	 * @param clazz the type of the target object
+	 * @return Java object that the Firestore document was concerted to
+	 */
 	<T> T convertToCustomClass(Document document, Class<T> clazz);
 }
