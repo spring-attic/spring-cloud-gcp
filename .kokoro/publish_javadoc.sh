341157d392c3ce22517655f7cab157970fae35f3
dzou <dzou@users.noreply.github.com>
2020-01-17 16:41:31 -0500

    write pwd.

--- .kokoro/publish_javadoc.sh
+++ .kokoro/publish_javadoc.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -eo pipefail
 
+pwd
+
 dir=$(dirname "$0")
 
 pushd $dir/../
-./mvnw install -DskipTests
+./mvnw clean javadoc:aggregate
+pwd
 popd
+
+pwd
+# install docuploader package
+python3 -m pip install gcp-docuploader
