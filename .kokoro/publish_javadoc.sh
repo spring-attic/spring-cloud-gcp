d626010dcee4a0eec5ee55aa04f887e9235b2d5a
dzou <dzou@users.noreply.github.com>
2020-01-21 14:15:52 -0500

    add more changes

--- .kokoro/publish_javadoc.sh
+++ .kokoro/publish_javadoc.sh
@@ -5,10 +5,22 @@ set -eo pipefail
 dir=$(dirname "$0")
 pushd $dir/../
 
+# Compute the project version.
+PROJECT_VERSION=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
+
 # install docuploader package
 python3 -m pip install --user gcp-docuploader
 
+# Build the javadocs
+./mvnw clean javadoc:aggregate
 
+# Move into generated docs directory
+pushd target/site/apidocs/
+
+python3 -m docuploader create-metadata \
+    --name spring-cloud-gcp \
+    --version ${PROJECT_VERSION} \
+    --language java
 
 popd
-
+popd
