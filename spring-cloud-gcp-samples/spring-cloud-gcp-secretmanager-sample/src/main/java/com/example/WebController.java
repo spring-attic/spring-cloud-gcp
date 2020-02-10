a7deb46fce479c1ba286da168292dee412c78c6c
dzou <dzou@users.noreply.github.com>
2020-02-10 14:59:11 -0500

    Add Secret Manager Sample

---
+++ spring-cloud-gcp-samples/spring-cloud-gcp-secretmanager-sample/src/main/java/com/example/WebController.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+	@Autowired
+	private Environment environment;
+
+	@Autowired
+	private MyAppProperties properties;
+
+	@GetMapping("/")
+	public String getRoot() {
+		// In practice, you never want to print your secrets as plaintext.
+		return "Hello World. The secret property is: " + properties.getSecret();
+	}
+}
