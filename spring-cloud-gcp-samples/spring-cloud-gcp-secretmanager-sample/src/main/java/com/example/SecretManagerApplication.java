a632126c6941ea774a283db5fa4a8f41097714d0
dzou <dzou@users.noreply.github.com>
2020-04-16 15:35:55 -0400

    Fix checkstyle

--- spring-cloud-gcp-samples/spring-cloud-gcp-secretmanager-sample/src/main/java/com/example/SecretManagerApplication.java
+++ spring-cloud-gcp-samples/spring-cloud-gcp-secretmanager-sample/src/main/java/com/example/SecretManagerApplication.java
@@ -18,7 +18,6 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class SecretManagerApplication {
