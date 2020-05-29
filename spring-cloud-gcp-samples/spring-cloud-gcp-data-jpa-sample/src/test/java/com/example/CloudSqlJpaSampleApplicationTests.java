fb04a1ae6048ef3cc9d92713fb417699c637aba2
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-05-29 10:37:57 +0200

    Changed the deprecated output capture rule

--- spring-cloud-gcp-samples/spring-cloud-gcp-data-jpa-sample/src/test/java/com/example/CloudSqlJpaSampleApplicationTests.java
+++ spring-cloud-gcp-samples/spring-cloud-gcp-data-jpa-sample/src/test/java/com/example/CloudSqlJpaSampleApplicationTests.java
@@ -24,7 +24,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.rule.OutputCaptureRule;
+import org.springframework.boot.test.system.OutputCaptureRule;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
