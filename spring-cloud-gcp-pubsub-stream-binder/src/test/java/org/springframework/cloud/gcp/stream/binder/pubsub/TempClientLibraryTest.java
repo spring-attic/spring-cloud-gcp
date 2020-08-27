03e97375a463599e7e06ea8f5c9db5d51aa800b1
Elena Felder <41136058+elefeint@users.noreply.github.com>
2020-08-27 19:52:50 -0400

    override retry settings in test copy

--- spring-cloud-gcp-pubsub-stream-binder/src/test/java/org/springframework/cloud/gcp/stream/binder/pubsub/TempClientLibraryTest.java
+++ spring-cloud-gcp-pubsub-stream-binder/src/test/java/org/springframework/cloud/gcp/stream/binder/pubsub/TempClientLibraryTest.java
@@ -1,7 +1,11 @@
 package org.springframework.cloud.gcp.stream.binder.pubsub;
 
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import com.google.api.core.ApiFutureCallback;
 import com.google.api.core.ApiFutures;
+import com.google.api.gax.retrying.RetrySettings;
 import com.google.cloud.pubsub.v1.Publisher;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.PubsubMessage;
@@ -9,12 +13,10 @@ import org.awaitility.Awaitility;
 import org.awaitility.Duration;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicBoolean;
-
 public class TempClientLibraryTest {
 
+	/* This test fails on timeout: because client library retries indefinitely, the failed publish never calls
+	* the onFailure callback. */
 	@Test
 	public void failedPublishCallsListener() throws IOException, InterruptedException {
 		Publisher publisher = Publisher.newBuilder("projects/elfel-spring/topics/exampleTopic")
@@ -47,6 +49,47 @@ public class TempClientLibraryTest {
 
 	}
 
+
+	/* This test is identical to the one above, but with overridden retry settings.
+	Therefore, it works as expected, invoking the onFailure callback. */
+	@Test
+	public void failedPublishCallsListener_overriddenRetrySettingsAllowsAsyncPublishToProperlyFail() throws IOException, InterruptedException {
+		Publisher publisher = Publisher.newBuilder("projects/elfel-spring/topics/exampleTopic")
+				.setEndpoint("cecicestnespasunendpoint:443")
+				.setRetrySettings(
+						RetrySettings.newBuilder()
+								.setMaxAttempts(3)
+								.setTotalTimeout(org.threeten.bp.Duration.ofSeconds(10))
+								.setInitialRpcTimeout(org.threeten.bp.Duration.ofSeconds(10))
+								.setMaxRpcTimeout(org.threeten.bp.Duration.ofSeconds(20)) // TODO: bad message; try 9; longer not shorter
+								.build())
+				.build();
+
+		PubsubMessage message = PubsubMessage.newBuilder()
+				.setData(ByteString.copyFromUtf8("test message"))
+				.build();
+
+		AtomicBoolean errorSet = new AtomicBoolean(false);
+
+		ApiFutures.addCallback(publisher.publish(message),
+				new ApiFutureCallback<String>() {
+					@Override
+					public void onFailure(Throwable throwable) {
+						System.out.println("Sending throwable to error channel: " + throwable);
+						errorSet.set(true);
+					}
+
+					@Override
+					public void onSuccess(String messageId) {
+						System.out.println("Successfully published message " + messageId);
+					}
+				});
+
+		Awaitility.await()
+				.atMost(Duration.TWO_MINUTES)
+				.until(() -> errorSet.get());
+
+	}
 
 	@Test
 	public void successfulPublishCallsListener() throws IOException, InterruptedException {
