f92618c91298fad1ef1f586d49eaa2d1da87a128
Mike Eltsufin <meltsufin@google.com>
2020-09-18 17:46:27 -0400

    Fix logging filter syntax from `and` to `AND` (#2527)

--- spring-cloud-gcp-samples/spring-cloud-gcp-trace-sample/src/test/java/com/example/TraceSampleApplicationTests.java
+++ spring-cloud-gcp-samples/spring-cloud-gcp-trace-sample/src/test/java/com/example/TraceSampleApplicationTests.java
@@ -132,7 +132,7 @@ public class TraceSampleApplicationTests {
 				.build();
 
 		String logFilter = String.format(
-				"trace=projects/%s/traces/%s and logName=projects/%s/logs/spring.log and timestamp>=\"%s\"",
+				"trace=projects/%s/traces/%s AND logName=projects/%s/logs/spring.log AND timestamp>=\"%s\"",
 				this.projectIdProvider.getProjectId(), uuidString,
 				this.projectIdProvider.getProjectId(), startDateTime.toStringRfc3339());
 
