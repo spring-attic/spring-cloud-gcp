d293873fa660a75c930cdd50a3859df67c011803
Travis Tomsu <ttomsu@google.com>
2020-11-18 15:53:01 -0500

    chore(builds): Replace  TravisCI status badge with GHAction ones (#2596)

--- docs/generate-docs.sh
+++ docs/generate-docs.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -x
 
+# Something in this stack doesn't work with Java 11.
+#
+# $ update-java-alternatives --list
+# $ sudo update-java-alternatives --set $JAVA_8_JDK
+#
+# Example:
+# $ sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
+#
+# Don't forget to set it back!
+
 # This command regenerates the docs after editing.
 # You'll have to run this after editing the src/main/asciidoc/README.adoc
 
